049e1e229a49134892134ce3a89e5f406f49d6d6
Tarsis de Lima <br.tmvdl@gmail.com>
2023-02-20 15:43:52 -0300

    Fixing 'GitHub Pages' workflow

--- env/projects.sh
+++ env/projects.sh
@@ -1,24 +1,22 @@
 REPOS=$( curl -sSL 'https://api.github.com/users/brtmvdl/repos?limit=200&source=1' | jq '.[].full_name' | sed -e 's/"//ig' )
 
-GITHUB_WORKSPACE="tmp"
+TEMPDIR="tmp"
 
-rm -rf "${GITHUB_WORKSPACE}" "projects/"
-
-mkdir -p "${GITHUB_WORKSPACE}" "projects/"
+mkdir -p "${TEMPDIR}" "projects/"
 
 for REPO in $REPOS; do
 
-  REPO_PATH="${GITHUB_WORKSPACE}/${REPO}"
-
-  echo
-  echo
-  echo REPO_PATH: "${REPO_PATH}"
-  echo
-  echo
+  REPO_PATH="${TEMPDIR}/${REPO}"
 
   echo gh repo clone "${REPO}" "${REPO_PATH}"
 
   gh repo clone "${REPO}" "${REPO_PATH}"
+
+done
+
+for REPO in $( ls tmp/brtmvdl/ ); do
+
+  REPO_PATH="${TEMPDIR}/${REPO}"
 
   echo cp -rfv "tmp/brtmvdl/${REPO}/src/github.io" "projects/${REPO}"
 
