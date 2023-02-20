0a74f5758bfc0d8429d42243183cc5addd02751a
Tarsis de Lima <br.tmvdl@gmail.com>
2023-02-20 15:26:09 -0300

    Adding 'GitHub Pages' workflow file

---
+++ env/projects.sh
@@ -0,0 +1,27 @@
+REPOS=$( curl -sSL 'https://api.github.com/users/brtmvdl/repos?limit=200&source=1' | jq '.[].full_name' | sed -e 's/"//ig' )
+
+GITHUB_WORKSPACE="tmp"
+
+rm -rf "${GITHUB_WORKSPACE}" "projects/"
+
+mkdir -p "${GITHUB_WORKSPACE}" "projects/"
+
+for REPO in $REPOS; do
+
+  REPO_PATH="${GITHUB_WORKSPACE}/${REPO}"
+
+  echo
+  echo
+  echo REPO_PATH: "${REPO_PATH}"
+  echo
+  echo
+
+  echo gh repo clone "${REPO}" "${REPO_PATH}"
+
+  gh repo clone "${REPO}" "${REPO_PATH}"
+
+  echo cp -rfv "tmp/brtmvdl/${REPO}/src/github.io" "projects/${REPO}"
+
+  cp -rfv "tmp/brtmvdl/${REPO}/src/github.io" "projects/${REPO}"
+
+done
