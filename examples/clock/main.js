09ef84f49f48cd19e82b7fd820115bcf855ddb24
Tarsis <br.tmvdl@gmail.com>
2023-04-04 15:02:12 +0000

    Fixing CLOCK files

--- examples/clock/main.js
+++ examples/clock/main.js
@@ -10,106 +10,95 @@ const gui = new dat.GUI({ name: 'Clock' })
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 1500)
-camera.position.set(0, 10, 10)
+camera.position.set(-10, 20, 0)
 camera.lookAt(0, 0, 0)
 
 const renderer = new THREE.WebGLRenderer()
+renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.update()
 
-const box = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1),
-  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-)
-box.position.set(0, 0, -Math.PI / 2)
-scene.add(box)
-
-const gridHelper = new THREE.GridHelper(7, 70)
+const gridHelper = new THREE.GridHelper(10, 100)
 scene.add(gridHelper)
 
-const edge = new THREE.Mesh(
-  new THREE.CylinderGeometry(3, 2.75, 0.999),
+const origin = new THREE.Mesh(
+  new THREE.CylinderGeometry(Math.PI / 16, .1, 1),
   new THREE.MeshBasicMaterial({ color: 0x333333 })
 )
-scene.add(edge)
+origin.position.set(0, 2 / 5, 0)
+scene.add(origin)
 
-const circle = new THREE.Mesh(
-  new THREE.CylinderGeometry(2.75, 2.75, 1),
+const clock = new THREE.Mesh(
+  new THREE.CylinderGeometry(Math.PI * 18 / 12, 2.75, 0.999),
+  new THREE.MeshBasicMaterial({ color: 0x333333 })
+)
+scene.add(clock)
+
+const edge = new THREE.Mesh(
+  new THREE.CylinderGeometry(Math.PI * 16 / 12, 2.75, 1),
   new THREE.MeshBasicMaterial({ color: 0xFFFFFF })
 )
-edge.add(circle)
+clock.add(edge)
 
 const hourPointer = new THREE.Mesh(
   new THREE.CylinderGeometry(.05, .01, 4),
-  new THREE.MeshBasicMaterial({ color: 0x990000 }),
+  new THREE.MeshBasicMaterial({ color: 0x666666 }),
 )
-hourPointer.rotation.set(0, 0, Math.PI / 2)
+hourPointer.rotation.set(0, 0, -Math.PI / 2)
 hourPointer.position.set(0, 0.75, 0)
 scene.add(hourPointer)
 
 const minutePointer = new THREE.Mesh(
   new THREE.CylinderGeometry(.05, .01, 4),
-  new THREE.MeshBasicMaterial({ color: 0x009900 })
+  new THREE.MeshBasicMaterial({ color: 0x999999 })
 )
-minutePointer.rotation.set(0, 0, Math.PI / 2)
+minutePointer.rotation.set(0, 0, -Math.PI / 2)
 minutePointer.position.set(0, 0.75, 0)
 scene.add(minutePointer)
 
 const secondPointer = new THREE.Mesh(
   new THREE.CylinderGeometry(.05, .01, 4),
-  new THREE.MeshBasicMaterial({ color: 0x000099 })
+  new THREE.MeshBasicMaterial({ color: 0xcccccc })
 )
-secondPointer.rotation.set(0, 0, Math.PI / 2)
+secondPointer.rotation.set(0, 0, -Math.PI / 2)
 secondPointer.position.set(0, 0.75, 0)
 scene.add(secondPointer)
 
 const fonts = {}
 
-const loadFont = (name) => {
-  return new Promise((resolve, reject) => {
-    if (fonts[name]) return resolve(fonts[name])
+const loadFont = (name) => new Promise((res, rej) => {
+  if (fonts[name]) return res(fonts[name])
+  const loader = new FontLoader()
+  loader.load(name, (font) => res(font), () => { }, (err) => rej(err))
+})
 
-    const loader = new FontLoader()
-    loader.load(
-      name,
-      (font) => resolve(font),
-      (xhr) => console.log({ xhr }),
-      (err) => reject(err),
-    )
-  })
-}
+const size = 1 / 2
+const height = 1 / 50
 
-loadFont(helvetiker)
-  .then((font) => {
-    const textGeometry = new TextGeometry('12', {
-      font: font,
-      size: 0.5,
-      height: 0.2,
-      curveSegments: 5,
-      bevelEnabled: true,
-      bevelThickness: 0.03,
-      bevelSize: 0.02,
-      bevelOffset: 0,
-      bevelSegments: 4
-    })
+const matcap = new THREE.TextureLoader().load('./ball.png')
 
-    textGeometry.center()
+Array.from(Array(12))
+  .forEach((_, angle) => loadFont(helvetiker)
+    .then((font) => {
+      const textGeometry = new TextGeometry(angle.toString(), { font, size, height })
+      textGeometry.center()
 
-    const text = new THREE.Mesh(
-      textGeometry,
-      new THREE.MeshMatcapMaterial({
-        matcap: new THREE.TextureLoader().load('./ball.png')
-      })
-    )
+      const textMesh = new THREE.Mesh(
+        textGeometry,
+        new THREE.MeshMatcapMaterial({ matcap })
+      )
+      textMesh.rotation.set(Math.PI * 3 / 2, 0, -Math.PI / 2)
 
-    text.rotation.set(Math.PI * 1.5, Math.PI * 0, Math.PI * 0)
-    text.position.set(0, 0.75, -2)
+      const hyp = Math.PI
+      textMesh.position.x = hyp * Math.cos(angle * Math.PI / 6)
+      textMesh.position.y = 0.75
+      textMesh.position.z = hyp * Math.sin(angle * Math.PI / 6)
 
-    scene.add(text)
-  })
+      scene.add(textMesh)
+    }))
 
 const degreeAngle = (angle) => 2 * Math.PI * angle
 
