bb31a8f70f70cb739a7b9e8e4256518f8916a3b4
Tarsis <br.tmvdl@gmail.com>
2023-05-22 23:21:15 -0300

    Adding libraries files

---
+++ libs/experiences.js
@@ -0,0 +1,25 @@
+
+const xp = [
+  {
+    title: 'title 1',
+    image: 'image 1',
+    link: 'link 1',
+    type: 'type 1',
+  },
+  {
+    title: 'title 2',
+    image: 'image 2',
+    link: 'link 2',
+    type: 'type 2',
+    dates: [['2022', '05'], ['2023', '01']],
+  },
+  {
+    title: 'title 3',
+    image: 'image 3',
+    link: 'link 3',
+    type: 'type 3',
+    dates: [['2020', '05'], ['2022', '01']],
+  },
+]
+
+export default xp
