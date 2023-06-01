ed13b444865511d3a1f446877dd09177634709bb
Tarsis <br.tmvdl@gmail.com>
2023-06-01 17:53:02 -0300

    Updating experiences list

--- libs/experiences.js
+++ libs/experiences.js
@@ -1,24 +1,15 @@
 
+const TYPES = {
+  JOB: 'job',
+  EDUCATION: 'education',
+}
+
 const xp = [
   {
-    title: 'title 1',
-    image: 'image 1',
-    link: 'link 1',
-    type: 'type 1',
-  },
-  {
-    title: 'title 2',
-    image: 'image 2',
-    link: 'link 2',
-    type: 'type 2',
-    dates: [['2022', '05'], ['2023', '01']],
-  },
-  {
-    title: 'title 3',
-    image: 'image 3',
-    link: 'link 3',
-    type: 'type 3',
-    dates: [['2020', '05'], ['2022', '01']],
+    title: 'piano',
+    image: '/img/piano.png',
+    link: 'https://piano-brtmvdl.surge.sh/',
+    type: TYPES.JOB,
   },
 ]
 
