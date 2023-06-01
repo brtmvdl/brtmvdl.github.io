95113cad6b8f445d898f72ec1bc824dd8ad1a284
Tarsis <br.tmvdl@gmail.com>
2023-06-01 20:59:33 -0300

    Updating experiences list

--- libs/experiences.js
+++ libs/experiences.js
@@ -9,7 +9,16 @@ const xp = [
     title: 'piano',
     image: '/img/piano.png',
     link: 'https://piano-brtmvdl.surge.sh/',
-    type: TYPES.JOB,
+  },
+  {
+    title: 'namazon',
+    image: '/img/namazon.png',
+    link: 'http://namazon-brtmvdl.surge.sh',
+  },
+  {
+    title: 'nume',
+    image: '/img/nume.png',
+    link: 'https://nume-brtmvdl.surge.sh',
   },
 ]
 
