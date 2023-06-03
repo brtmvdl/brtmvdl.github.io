9469cc6fd79dfd55588eedf1e7e73a117f40d3cb
Tarsis <br.tmvdl@gmail.com>
2023-06-03 14:50:15 -0300

    Updating project list

--- libs/experiences.js
+++ libs/experiences.js
@@ -7,71 +7,83 @@ const TYPES = {
 const xp = [
   {
     "title": "airplane",
+    "image": "/img/airplane.png",
     "link": "https://airplane-brtmvdl.surge.sh"
   },
   {
     "title": "Create nElement App",
+    "image": "/img/create-nelement-app.png",
     "link": "https://www.npmjs.com/package/create-nelement-app"
   },
   {
     "title": "docker alpine",
+    "image": "/img/docker-alpine.png",
     "link": "https://hub.docker.com/r/tmvdl/alpine"
   },
   {
     "title": "docker android",
+    "image": "/img/docker-android.png",
     "link": "https://hub.docker.com/r/tmvdl/android"
   },
   {
     "title": "docker dart",
+    "image": "/img/docker-dart.png",
     "link": "https://hub.docker.com/r/tmvdl/dart"
   },
   {
     "title": "docker dotnet",
+    "image": "/img/docker-dotnet.png",
     "link": "https://hub.docker.com/r/tmvdl/dotnet"
   },
   {
     "title": "docker downloads",
+    "image": "/img/docker-downloads.png",
     "link": "https://hub.docker.com/r/tmvdl/downloads"
   },
   {
     "title": "docker go",
+    "image": "/img/docker-go.png",
     "link": "https://hub.docker.com/r/tmvdl/go"
   },
   {
     "title": "docker java",
+    "image": "/img/docker-java.png",
     "link": "https://hub.docker.com/r/tmvdl/java"
   },
   {
     "title": "docker kotlin",
+    "image": "/img/docker-kotlin.png",
     "link": "https://hub.docker.com/r/tmvdl/kotlin"
   },
   {
     "title": "docker node",
+    "image": "/img/docker-node.png",
     "link": "https://hub.docker.com/r/tmvdl/node"
   },
   {
     "title": "docker php",
+    "image": "/img/docker-php.png",
     "link": "https://hub.docker.com/r/tmvdl/php"
   },
   {
     "title": "docker projects",
+    "image": "/img/docker-projects.png",
     "link": "https://hub.docker.com/r/tmvdl/projects"
   },
   {
     "title": "docker python",
+    "image": "/img/docker-python.png",
     "link": "https://hub.docker.com/r/tmvdl/python"
   },
   {
     "title": "docker ruby",
+    "image": "/img/docker-ruby.png",
     "link": "https://hub.docker.com/r/tmvdl/ruby"
   },
   {
     "title": "yt-docker dl",
+    "image": "/img/docker-yt-dl.png",
     "link": "https://hub.docker.com/r/tmvdl/yt-dl"
-  },
-  {
-    "title": "",
-    "link": "https://github.com/actions"
   },
   {
     "title": "Visual Studio Code Front-End Extension Pack",
@@ -80,10 +92,12 @@ const xp = [
   },
   {
     "title": "nume",
+    "image": "/img/nume.png",
     "link": "https://nume-brtmvdl.surge.sh/"
   },
   {
     "title": "piano",
+    "image": "/img/piano.png",
     "link": "https://piano-brtmvdl.surge.sh/",
   }
 ]
