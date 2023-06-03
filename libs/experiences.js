aef9f03fae48325ef16ce4efe3cf37e31e8eec86
Tarsis <br.tmvdl@gmail.com>
2023-06-03 11:29:40 -0300

    Updating project list

--- libs/experiences.js
+++ libs/experiences.js
@@ -10,38 +10,38 @@ const xp = [
     image: '/img/piano.png',
     link: 'https://piano-brtmvdl.surge.sh/',
   },
-  {
-    title: 'namazon',
-    image: '/img/namazon.png',
-    link: 'http://namazon-brtmvdl.surge.sh',
-  },
+  // {
+  //   title: 'namazon',
+  //   image: '/img/namazon.png',
+  //   link: 'http://namazon-brtmvdl.surge.sh',
+  // },
   {
     title: 'nume',
     image: '/img/nume.png',
     link: 'https://nume-brtmvdl.surge.sh',
   },
+  // {
+  //   title: 'hours',
+  //   image: '/img/hours.png',
+  //   link: 'http://hours-www.vercel.app'
+  // },
+  // {
+  //   title: 'buildbox',
+  //   image: '/img/buildbox.png',
+  //   link: 'http://buildbox-www.vercel.app'
+  // },
+  // {
+  //   title: 'ubistart-frontend-challenge',
+  //   image: '/img/ubistart-frontend-challenge.png',
+  //   link: 'https://ubistart-frontend-challenge.vercel.app'
+  // },
+  // {
+  //   title: 'aws-nextjs-three',
+  //   image: '/img/aws-nextjs-three.png',
+  //   link: 'https://aws-nextjs-three.vercel.app'
+  // },
   {
-    title: 'hours',
-    image: '/img/hours.png',
-    link: 'http://hours-www.vercel.app'
-  },
-  {
-    title: 'buildbox',
-    image: '/img/buildbox.png',
-    link: 'http://buildbox-www.vercel.app'
-  },
-  {
-    title: 'ubistart-frontend-challenge',
-    image: '/img/ubistart-frontend-challenge.png',
-    link: 'https://ubistart-frontend-challenge.vercel.app'
-  },
-  {
-    title: 'aws-nextjs-three',
-    image: '/img/aws-nextjs-three.png',
-    link: 'https://aws-nextjs-three.vercel.app'
-  },
-  {
-    title: 'frontend-extension-pack-vsce',
+    title: 'Visual Studio Code Front-End Extension Pack',
     image: '/img/frontend-extension-pack-vsce.png',
     link:'https://marketplace.visualstudio.com/items?itemName=tmvdl.frontend-extension-pack-vsce'
   },
