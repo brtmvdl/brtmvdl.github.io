52fab64771101e7ac5202fe2206a7ba63fbfb88b
Tarsis <br.tmvdl@gmail.com>
2023-06-03 12:05:30 -0300

    Updating project list

--- libs/experiences.js
+++ libs/experiences.js
@@ -1,51 +1,91 @@
 
 const TYPES = {
-  JOB: 'job',
-  EDUCATION: 'education',
+  JOB: "job",
+  EDUCATION: "education",
 }
 
 const xp = [
   {
-    title: 'piano',
-    image: '/img/piano.png',
-    link: 'https://piano-brtmvdl.surge.sh/',
+    "title": "airplane",
+    "url": "https://airplane-brtmvdl.surge.sh"
   },
-  // {
-  //   title: 'namazon',
-  //   image: '/img/namazon.png',
-  //   link: 'http://namazon-brtmvdl.surge.sh',
-  // },
   {
-    title: 'nume',
-    image: '/img/nume.png',
-    link: 'https://nume-brtmvdl.surge.sh',
+    "title": "Create nElement App",
+    "url": "https://www.npmjs.com/package/create-nelement-app"
   },
-  // {
-  //   title: 'hours',
-  //   image: '/img/hours.png',
-  //   link: 'http://hours-www.vercel.app'
-  // },
-  // {
-  //   title: 'buildbox',
-  //   image: '/img/buildbox.png',
-  //   link: 'http://buildbox-www.vercel.app'
-  // },
-  // {
-  //   title: 'ubistart-frontend-challenge',
-  //   image: '/img/ubistart-frontend-challenge.png',
-  //   link: 'https://ubistart-frontend-challenge.vercel.app'
-  // },
-  // {
-  //   title: 'aws-nextjs-three',
-  //   image: '/img/aws-nextjs-three.png',
-  //   link: 'https://aws-nextjs-three.vercel.app'
-  // },
   {
-    title: 'Visual Studio Code Front-End Extension Pack',
-    image: '/img/frontend-extension-pack-vsce.png',
-    link:'https://marketplace.visualstudio.com/items?itemName=tmvdl.frontend-extension-pack-vsce'
+    "title": "docker alpine",
+    "url": "https://hub.docker.com/r/tmvdl/alpine"
   },
+  {
+    "title": "docker android",
+    "url": "https://hub.docker.com/r/tmvdl/android"
+  },
+  {
+    "title": "docker dart",
+    "url": "https://hub.docker.com/r/tmvdl/dart"
+  },
+  {
+    "title": "docker dotnet",
+    "url": "https://hub.docker.com/r/tmvdl/dotnet"
+  },
+  {
+    "title": "docker downloads",
+    "url": "https://hub.docker.com/r/tmvdl/downloads"
+  },
+  {
+    "title": "docker go",
+    "url": "https://hub.docker.com/r/tmvdl/go"
+  },
+  {
+    "title": "docker java",
+    "url": "https://hub.docker.com/r/tmvdl/java"
+  },
+  {
+    "title": "docker kotlin",
+    "url": "https://hub.docker.com/r/tmvdl/kotlin"
+  },
+  {
+    "title": "docker node",
+    "url": "https://hub.docker.com/r/tmvdl/node"
+  },
+  {
+    "title": "docker php",
+    "url": "https://hub.docker.com/r/tmvdl/php"
+  },
+  {
+    "title": "docker projects",
+    "url": "https://hub.docker.com/r/tmvdl/projects"
+  },
+  {
+    "title": "docker python",
+    "url": "https://hub.docker.com/r/tmvdl/python"
+  },
+  {
+    "title": "docker ruby",
+    "url": "https://hub.docker.com/r/tmvdl/ruby"
+  },
+  {
+    "title": "yt-docker dl",
+    "url": "https://hub.docker.com/r/tmvdl/yt-dl"
+  },
+  {
+    "title": "",
+    "url": "https://github.com/actions"
+  },
+  {
+    "title": "Visual Studio Code Front-End Extension Pack",
+    "image": "/img/frontend-extension-pack-vsce.png",
+    "link": "https://marketplace.visualstudio.com/items?itemName=tmvdl.frontend-extension-pack-vsce"
+  },
+  {
+    "title": "nume",
+    "url": "https://nume-brtmvdl.surge.sh/"
+  },
+  {
+    "title": "piano",
+    "url": "https://piano-brtmvdl.surge.sh/",
+  }
 ]
 
 export default xp
-
