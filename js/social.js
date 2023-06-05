a5e6f43684f937a0fb977ebc09224e24891ed5be
Tarsis <br.tmvdl@gmail.com>
2023-06-05 10:22:49 -0300

    Adding Social model file

---
+++ js/social.js
@@ -0,0 +1,35 @@
+import { nElement, nImage, nLink } from './nElement.js'
+
+export class Social {
+  title = ''
+  image = ''
+  link = ''
+
+  constructor({
+    title = '',
+    image = '',
+    link = '',
+  } = {}) {
+    this.title = title
+    this.image = image
+    this.link = link
+  }
+
+  getListItem() {
+    const el = new nElement()
+    el.setStyle('margin', '1em 0em')
+    el.setStyle('width', '4em')
+
+    const link = new nLink()
+    link.href(this.link)
+    el.append(link)
+
+    const image = new nImage()
+    image.src(this.image)
+    image.alt(this.title)
+    link.append(image)
+
+    return el
+  }
+
+}
