97b63295ba0338d0a84761068095660af5885388
Tarsis <br.tmvdl@gmail.com>
2023-06-05 10:23:23 -0300

    Updating INDEX page

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import { nElement, nH1, nH2, nHR, nLink } from './js/nElement.js'
+import { nElement, nFlex, nH1 } from './js/nElement.js'
 import { Experience } from './js/experience.js'
+import { Social } from './js/social.js'
 
 import experiences from './libs/experiences.js'
+import socials from './libs/socials.js'
 
 nElement.fromElement(document.body)
   .setStyle('margin', '0rem')
@@ -23,6 +25,14 @@ const body = new nElement()
 body.setStyle('margin', '1rem auto')
 body.setStyle('width', '40rem')
 app.append(body)
+
+const socialsEl = new nFlex()
+body.append(socialsEl)
+
+socials
+  .map((s = {}) => new Social(s))
+  .sort((s1, s2) => s2.title.localeCompare(s1.title))
+  .map((s) => socialsEl.append(s.getListItem()))
 
 const experiencesEl = new nElement()
 body.append(experiencesEl)
