26bf331845accd54d38540e1c729484969f64f26
Tarsis <br.tmvdl@gmail.com>
2023-06-06 11:01:40 -0300

    Updating Calc example files

--- examples/calc/index.js
+++ examples/calc/index.js
@@ -1,4 +1,4 @@
-import { nButton, nElement, nFlex, nH1, nH2, nInputTextGroup, nSelectGroup } from '../../js/nElement.js'
+import { nButton, nElement, nFlex, nH1, nH2, nImage, nInputTextGroup, nSelectGroup } from '../../js/nElement.js'
 import * as COLORS from '../../libs/colors.js'
 
 nElement.fromElement(document.body)
@@ -33,14 +33,21 @@ dInput.input.setStyle('padding', '1em 0em')
 dInput.error.setStyle('padding', '1em 0em')
 domain.append(dInput)
 
+const periods = {
+  '01': '1 year',
+  '03': '3 years',
+  '05': '5 years',
+  '10': '10 years',
+}
+
 const dPeriod = new nSelectGroup()
 dPeriod.label.setText('Contract:')
 dPeriod.select.setStyle('padding', '1em 0em')
 dPeriod.select.setStyle('background-color', COLORS.WHITE)
-dPeriod.select.addOption('01', '1 year')
-dPeriod.select.addOption('03', '3 year')
-dPeriod.select.addOption('05', '5 year')
-dPeriod.select.addOption('10', '10 year')
+dPeriod.select.addOption('01', periods['01'])
+dPeriod.select.addOption('03', periods['03'])
+dPeriod.select.addOption('05', periods['05'])
+dPeriod.select.addOption('10', periods['10'])
 domain.append(dPeriod)
 
 const addProjectButton = new nButton()
@@ -65,11 +72,58 @@ addProjectButton.on('click', () => {
   updateProjects(projects)
 
   dInput.input.setValue('')
-  dPeriod.setValue('')
+  dPeriod.select.setValue('01')
 })
 body.append(addProjectButton)
 
-const projects = []
+const projects = [
+]
 
 const projectsEl = new nElement()
 body.append(projectsEl)
+
+const updateProjects = () => {
+  projectsEl.clear()
+
+  if (projects.length > 0) {
+    projects.map((p, ix) => {
+      const proj = new nElement()
+      proj.setStyle('box-shadow', ['0em', '0em', '0em', 'calc(1em / 8)', COLORS.BLACK].join(' '))
+      proj.setStyle('margin', '1em 0em')
+      proj.setStyle('padding', '1em')
+
+      const projHeader = new nFlex()
+      proj.append(projHeader)
+
+      const projTitle = new nElement()
+      projTitle.setText(`${p.domain} - ${periods[p.period]}`)
+      projHeader.append(projTitle)
+
+      const projDelete = new nElement()
+      projDelete.setText('delete')
+      projDelete.setStyle('color', COLORS.RED)
+      projDelete.setStyle('cursor', 'pointer')
+      projDelete.on('click', () => {
+        const projs = projects.filter((_, pIx) => pIx != ix)
+        while (projects.length > 0) projects.pop()
+        projs.map((p) => projects.push(p))
+
+        updateProjects()
+      })
+      projHeader.append(projDelete)
+
+      const projBody = new nElement()
+      proj.append(projBody)
+
+      projectsEl.append(proj)
+    })
+  } else {
+    const noProjectst = new nElement()
+    noProjectst.setText('no projects')
+    noProjectst.setStyle('padding', '1em 0em')
+    noProjectst.setStyle('text-align', 'center')
+    projectsEl.append(noProjectst)
+  }
+}
+
+updateProjects(projects)
