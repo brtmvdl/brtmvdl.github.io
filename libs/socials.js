0508c1f85b202f4e2a59a514f67ad833a8013567
Tarsis <br.tmvdl@gmail.com>
2023-06-20 16:07:01 -0300

    Adding Surge.sh url file

--- libs/socials.js
+++ libs/socials.js
@@ -5,13 +5,18 @@ export default [
     "link": "https://github.com/brtmvdl?tab=repositories"
   },
   {
+    "title": "Twitter",
+    "image": "/img/twitter.svg",
+    "link": "https://twitter.com/brtmvdl"
+  },
+  {
     "title": "Linkedin",
     "image": "/img/linkedin.svg",
     "link": "https://www.linkedin.com/in/brtmvdl"
   },
   {
-    "title": "Twitter",
-    "image": "/img/twitter.svg",
-    "link": "https://twitter.com/brtmvdl"
+    "title": "Amazon",
+    "image": "/img/amazon.svg",
+    "link": "https://www.amazon.com.br/hz/wishlist/ls/QUI8HXVLO72H?ref_=wl_share"
   },
 ]
