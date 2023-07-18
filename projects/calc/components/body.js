344d15cdca58b554dca395d0d75e7fff89374a93
Tarsis <br.tmvdl@gmail.com>
2023-07-18 16:05:43 -0300

    Updating components files

---
+++ projects/calc/components/body.js
@@ -0,0 +1,7 @@
+import { nElement } from '../nElement.js'
+
+export class BodyComponent extends nElement {
+  getName() {
+    return 'body-component'
+  }
+}
---
+++ projects/calc/components/head.js
@@ -0,0 +1,7 @@
+import { nElement } from '../nElement.js'
+
+export class HeadComponent extends nElement {
+  getName() {
+    return 'head-component'
+  }
+}
