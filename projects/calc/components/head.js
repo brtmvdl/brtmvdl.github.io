704b1c777ca458b3604af3be13ca560f6362aad1
Tarsis <br.tmvdl@gmail.com>
2023-07-18 16:09:39 -0300

    Updating components files

--- projects/calc/components/head.js
+++ projects/calc/components/head.js
@@ -1,7 +1,19 @@
 import { nElement } from '../nElement.js'
+import * as COLORS from '../../../libs/colors.js'
 
 export class HeadComponent extends nElement {
   getName() {
     return 'head-component'
+  }
+
+  onCreate() {
+    super.onCreate()
+
+    this.setText('CALC')
+
+    this.setStyle('background-color', COLORS.BLACK_1)
+    this.setStyle('color', COLORS.WHITE_1)
+    this.setStyle('text-align', 'center')
+    this.setStyle('padding', '1rem')
   }
 }
