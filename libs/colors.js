020e7304cf68ef7c299a212f88daaa6da0207fce
Tarsis <br.tmvdl@gmail.com>
2023-07-18 10:04:02 -0300

    Updating colors file

--- libs/colors.js
+++ libs/colors.js
@@ -1,7 +1,11 @@
-export const WHITE = '#ffffff'
+export const WHITE_1 = '#ffffff'
+export const WHITE_2 = '#cccccc'
 
-export const BLACK = '#000000'
+export const GRAY_1 = '#666666'
+export const GRAY_2 = '#999999'
 
-export const YELLOW = '#ff0000'
+export const BLACK_1 = '#000000'
+export const BLACK_2 = '#333333'
 
+export const YELLOW = '#ffff00'
 export const RED = '#ff0000'
