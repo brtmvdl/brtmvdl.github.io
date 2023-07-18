db73bcc90d842a0d7c7c90edcd5c08fc055a8a73
Tarsis <br.tmvdl@gmail.com>
2023-07-18 10:04:29 -0300

    Updating project list file

---
+++ libs/projects.js
@@ -0,0 +1,786 @@
+const projects = [
+  {
+    "project": "actions",
+    "title": ""
+  },
+  {
+    "project": "lyrics",
+    "title": ""
+  },
+  {
+    "project": "airplane",
+    "title": ""
+  },
+  {
+    "project": "magento",
+    "title": ""
+  },
+  {
+    "project": "ajax-in-aws",
+    "title": ""
+  },
+  {
+    "project": "mapping",
+    "title": ""
+  },
+  {
+    "project": "alunos-api",
+    "title": ""
+  },
+  {
+    "project": "market",
+    "title": ""
+  },
+  {
+    "project": "alunos-app",
+    "title": ""
+  },
+  {
+    "project": "maslick-koder",
+    "title": ""
+  },
+  {
+    "project": "anagram",
+    "title": ""
+  },
+  {
+    "project": "messenger",
+    "title": ""
+  },
+  {
+    "project": "antify",
+    "title": ""
+  },
+  {
+    "project": "metronomo",
+    "title": ""
+  },
+  {
+    "project": "antify-go",
+    "title": ""
+  },
+  {
+    "project": "micromodels",
+    "title": ""
+  },
+  {
+    "project": "api-nodejs-mongodb",
+    "title": ""
+  },
+  {
+    "project": "mobile-action",
+    "title": ""
+  },
+  {
+    "project": "apk",
+    "title": ""
+  },
+  {
+    "project": "money",
+    "title": ""
+  },
+  {
+    "project": "app",
+    "title": ""
+  },
+  {
+    "project": "mongo-database",
+    "title": ""
+  },
+  {
+    "project": "app-name",
+    "title": ""
+  },
+  {
+    "project": "nElement",
+    "title": ""
+  },
+  {
+    "project": "apply",
+    "title": ""
+  },
+  {
+    "project": "namazon",
+    "title": ""
+  },
+  {
+    "project": "audio-recorder",
+    "title": ""
+  },
+  {
+    "project": "napp-sample",
+    "title": ""
+  },
+  {
+    "project": "aws-examples",
+    "title": ""
+  },
+  {
+    "project": "ncontext",
+    "title": ""
+  },
+  {
+    "project": "aws-mac",
+    "title": ""
+  },
+  {
+    "project": "nelement",
+    "title": ""
+  },
+  {
+    "project": "aws-mac1.metal",
+    "title": ""
+  },
+  {
+    "project": "nelement-sample",
+    "title": ""
+  },
+  {
+    "project": "aws-nextjs",
+    "title": ""
+  },
+  {
+    "project": "ng-api",
+    "title": ""
+  },
+  {
+    "project": "aws-sns-sample",
+    "title": ""
+  },
+  {
+    "project": "ng-hd",
+    "title": ""
+  },
+  {
+    "project": "aws_ecr_sample",
+    "title": ""
+  },
+  {
+    "project": "nic-extension",
+    "title": ""
+  },
+  {
+    "project": "aws_serverless_sample",
+    "title": ""
+  },
+  {
+    "project": "node-docker-heroku",
+    "title": ""
+  },
+  {
+    "project": "back-end-challenge-2021",
+    "title": ""
+  },
+  {
+    "project": "nodejs",
+    "title": ""
+  },
+  {
+    "project": "ball",
+    "title": ""
+  },
+  {
+    "project": "nodejs-extension-pack-vsce",
+    "title": ""
+  },
+  {
+    "project": "barcode",
+    "title": ""
+  },
+  {
+    "project": "nono-app",
+    "title": ""
+  },
+  {
+    "project": "blendermodels",
+    "title": ""
+  },
+  {
+    "project": "numbers",
+    "title": ""
+  },
+  {
+    "project": "blog-old",
+    "title": ""
+  },
+  {
+    "project": "nume",
+    "title": ""
+  },
+  {
+    "project": "brtmvdl",
+    "title": ""
+  },
+  {
+    "project": "oowlish",
+    "title": ""
+  },
+  {
+    "project": "brtmvdl.github.io",
+    "title": ""
+  },
+  {
+    "project": "percent",
+    "title": ""
+  },
+  {
+    "project": "buildbox",
+    "title": ""
+  },
+  {
+    "project": "perguntas",
+    "title": ""
+  },
+  {
+    "project": "buildbox-www",
+    "title": ""
+  },
+  {
+    "project": "php",
+    "title": ""
+  },
+  {
+    "project": "busy",
+    "title": ""
+  },
+  {
+    "project": "piano",
+    "title": ""
+  },
+  {
+    "project": "calendar",
+    "title": ""
+  },
+  {
+    "project": "plain-html",
+    "title": ""
+  },
+  {
+    "project": "calendario-app",
+    "title": ""
+  },
+  {
+    "project": "plus-app",
+    "title": ""
+  },
+  {
+    "project": "calendario-front",
+    "title": ""
+  },
+  {
+    "project": "plus-www",
+    "title": ""
+  },
+  {
+    "project": "cento",
+    "title": ""
+  },
+  {
+    "project": "png",
+    "title": ""
+  },
+  {
+    "project": "cento-react",
+    "title": ""
+  },
+  {
+    "project": "pokedex",
+    "title": ""
+  },
+  {
+    "project": "chalkboard",
+    "title": ""
+  },
+  {
+    "project": "pomodoro",
+    "title": ""
+  },
+  {
+    "project": "chat-api",
+    "title": ""
+  },
+  {
+    "project": "portugues",
+    "title": ""
+  },
+  {
+    "project": "chess",
+    "title": ""
+  },
+  {
+    "project": "projeto",
+    "title": ""
+  },
+  {
+    "project": "chess-counter",
+    "title": ""
+  },
+  {
+    "project": "proxypass-nginx-server",
+    "title": ""
+  },
+  {
+    "project": "clead",
+    "title": ""
+  },
+  {
+    "project": "python",
+    "title": ""
+  },
+  {
+    "project": "clock",
+    "title": ""
+  },
+  {
+    "project": "qrcode",
+    "title": ""
+  },
+  {
+    "project": "clojure",
+    "title": ""
+  },
+  {
+    "project": "questionario",
+    "title": ""
+  },
+  {
+    "project": "cobol",
+    "title": ""
+  },
+  {
+    "project": "raining",
+    "title": ""
+  },
+  {
+    "project": "codeigniter",
+    "title": ""
+  },
+  {
+    "project": "raspberry-projects",
+    "title": ""
+  },
+  {
+    "project": "coodesh",
+    "title": ""
+  },
+  {
+    "project": "rent",
+    "title": ""
+  },
+  {
+    "project": "counter",
+    "title": ""
+  },
+  {
+    "project": "create-nelement-app",
+    "title": ""
+  },
+  {
+    "project": "ressignificar-api",
+    "title": ""
+  },
+  {
+    "project": "curriculus",
+    "title": ""
+  },
+  {
+    "project": "rhon",
+    "title": ""
+  },
+  {
+    "project": "cycle",
+    "title": ""
+  },
+  {
+    "project": "ruby",
+    "title": ""
+  },
+  {
+    "project": "dart",
+    "title": ""
+  },
+  {
+    "project": "rubyonrails",
+    "title": ""
+  },
+  {
+    "project": "dashboard",
+    "title": ""
+  },
+  {
+    "project": "sampleproject",
+    "title": ""
+  },
+  {
+    "project": "debian",
+    "title": ""
+  },
+  {
+    "project": "school",
+    "title": ""
+  },
+  {
+    "project": "desenhar",
+    "title": ""
+  },
+  {
+    "project": "screenshoot",
+    "title": ""
+  },
+  {
+    "project": "django-sample",
+    "title": ""
+  },
+  {
+    "project": "serversample-java",
+    "title": ""
+  },
+  {
+    "project": "domains-data",
+    "title": ""
+  },
+  {
+    "project": "singlepageapplication",
+    "title": ""
+  },
+  {
+    "project": "dotfiles",
+    "title": ""
+  },
+  {
+    "project": "slides3d",
+    "title": ""
+  },
+  {
+    "project": "dotnet",
+    "title": ""
+  },
+  {
+    "project": "speed",
+    "title": ""
+  },
+  {
+    "project": "drawer",
+    "title": ""
+  },
+  {
+    "project": "speed-app",
+    "title": ""
+  },
+  {
+    "project": "drupal",
+    "title": ""
+  },
+  {
+    "project": "spring-boot-demo",
+    "title": ""
+  },
+  {
+    "project": "edson-99freelas",
+    "title": ""
+  },
+  {
+    "project": "stroll",
+    "title": ""
+  },
+  {
+    "project": "env",
+    "title": ""
+  },
+  {
+    "project": "study",
+    "title": ""
+  },
+  {
+    "project": "fat-data",
+    "title": ""
+  },
+  {
+    "project": "svelte-sample",
+    "title": ""
+  },
+  {
+    "project": "fb-marketing-api",
+    "title": ""
+  },
+  {
+    "project": "tarsis.cc",
+    "title": ""
+  },
+  {
+    "project": "finagle",
+    "title": ""
+  },
+  {
+    "project": "tarsisdelima.github.io",
+    "title": ""
+  },
+  {
+    "project": "finance-app",
+    "title": ""
+  },
+  {
+    "project": "taskscreate-node-file",
+    "title": ""
+  },
+  {
+    "project": "find-text",
+    "title": ""
+  },
+  {
+    "project": "tasksget-node-file",
+    "title": ""
+  },
+  {
+    "project": "fireport",
+    "title": ""
+  },
+  {
+    "project": "taskslist-node-file",
+    "title": ""
+  },
+  {
+    "project": "flask-sample",
+    "title": ""
+  },
+  {
+    "project": "tasksupdate-node-file",
+    "title": ""
+  },
+  {
+    "project": "flutter",
+    "title": ""
+  },
+  {
+    "project": "terraform-aws",
+    "title": ""
+  },
+  {
+    "project": "frontend-extension-pack-vsce",
+    "title": ""
+  },
+  {
+    "project": "text",
+    "title": ""
+  },
+  {
+    "project": "games",
+    "title": ""
+  },
+  {
+    "project": "that",
+    "title": ""
+  },
+  {
+    "project": "geld",
+    "title": ""
+  },
+  {
+    "project": "threejs-samples",
+    "title": ""
+  },
+  {
+    "project": "ubistart",
+    "title": ""
+  },
+  {
+    "project": "ubistart-back",
+    "title": ""
+  },
+  {
+    "project": "ubistart-front",
+    "title": ""
+  },
+  {
+    "project": "go",
+    "title": ""
+  },
+  {
+    "project": "vestibular",
+    "title": ""
+  },
+  {
+    "project": "hidden",
+    "title": ""
+  },
+  {
+    "project": "vittae",
+    "title": ""
+  },
+  {
+    "project": "hours",
+    "title": ""
+  },
+  {
+    "project": "woocommerce",
+    "title": ""
+  },
+  {
+    "project": "hours-www",
+    "title": ""
+  },
+  {
+    "project": "wordpress",
+    "title": ""
+  },
+  {
+    "project": "how",
+    "title": ""
+  },
+  {
+    "project": "wrangler-sample",
+    "title": ""
+  },
+  {
+    "project": "html2json",
+    "title": ""
+  },
+  {
+    "project": "ws",
+    "title": ""
+  },
+  {
+    "project": "ideal",
+    "title": ""
+  },
+  {
+    "project": "yellowed",
+    "title": ""
+  },
+  {
+    "project": "ideal-www",
+    "title": ""
+  },
+  {
+    "project": "youchat",
+    "title": ""
+  },
+  {
+    "project": "imobiliario-adslist",
+    "title": ""
+  },
+  {
+    "project": "z-apis",
+    "title": ""
+  },
+  {
+    "project": "imobiliario-api",
+    "title": ""
+  },
+  {
+    "project": "z-blog-old",
+    "title": ""
+  },
+  {
+    "project": "imobiliario-back",
+    "title": ""
+  },
+  {
+    "project": "z-brasil-info",
+    "title": ""
+  },
+  {
+    "project": "imobiliario-front",
+    "title": ""
+  },
+  {
+    "project": "z-chat-dashboard",
+    "title": ""
+  },
+  {
+    "project": "imobiliario-old",
+    "title": ""
+  },
+  {
+    "project": "z-dotfiles",
+    "title": ""
+  },
+  {
+    "project": "iovel",
+    "title": ""
+  },
+  {
+    "project": "z-enqueteiro",
+    "title": ""
+  },
+  {
+    "project": "java",
+    "title": ""
+  },
+  {
+    "project": "z-events",
+    "title": ""
+  },
+  {
+    "project": "jobssender",
+    "title": ""
+  },
+  {
+    "project": "z-financeira",
+    "title": ""
+  },
+  {
+    "project": "jornal",
+    "title": ""
+  },
+  {
+    "project": "z-full-init",
+    "title": ""
+  },
+  {
+    "project": "jornal-old",
+    "title": ""
+  },
+  {
+    "project": "z-json",
+    "title": ""
+  },
+  {
+    "project": "json",
+    "title": ""
+  },
+  {
+    "project": "z-nini-web",
+    "title": ""
+  },
+  {
+    "project": "justeat",
+    "title": ""
+  },
+  {
+    "project": "z-pladados",
+    "title": ""
+  },
+  {
+    "project": "k8s",
+    "title": ""
+  },
+  {
+    "project": "z-projects-all",
+    "title": ""
+  },
+  {
+    "project": "laravel",
+    "title": ""
+  },
+  {
+    "project": "z-pushgap-app",
+    "title": ""
+  },
+  {
+    "project": "lines-app",
+    "title": ""
+  },
+  {
+    "project": "z-pushgap-web",
+    "title": ""
+  },
+  {
+    "project": "loja-old",
+    "title": ""
+  },
+  {
+    "project": "z-ressignificar-app",
+    "title": ""
+  },
+]
