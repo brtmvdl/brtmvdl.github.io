716bdb49bd9627b610c291f80bd9f47b040c4646
Tarsis <br.tmvdl@gmail.com>
2023-07-29 16:37:35 -0300

    Removing useless files

--- projects/calc-20230723/utils/validation.js
+++
@@ -1,7 +0,0 @@
-export const required = (errorMessage = 'This field is required.') => (value) => !!value ? null : errorMessage
-
-export const url = (errorMessage = 'This URL is invalid.') => (value) => {
-  const regexp = new RegExp('[a-z]+\.(com|net|org)(\.br)?')
-
-  return regexp.test(value) ? null : errorMessage
-}
