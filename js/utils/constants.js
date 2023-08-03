1e51306f619c18e9feff62aaf1a27e94b303e5a6
Tarsis <br.tmvdl@gmail.com>
2023-08-03 07:42:05 -0300

    Updating constants file

--- js/utils/constants.js
+++ js/utils/constants.js
@@ -2,6 +2,8 @@
 export const URLS = {
   PARAVOCE: '/para-voce/',
   PARAVOCE_CURSOS: '/para-voce/cursos/',
+  PARAVOCE_APPS: '/para-voce/apps/',
+  PARAVOCE_ATENDIMENTO: '/para-voce/atendimento/',
   PARAEMPRESAS: '/para-empresas/',
   PARADESENVOLVEDORES: '/para-desenvolvedores/',
   MONTEOSEU: '/monte-o-seu/',
