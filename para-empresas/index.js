dcbd648162b92ef8d2d1dbee5533f81352a1afdd
Tarsis <br.tmvdl@gmail.com>
2023-08-03 07:51:35 -0300

    Updating PARA EMPRESAS page files

--- para-empresas/index.js
+++ para-empresas/index.js
@@ -1,17 +1,18 @@
-import { nElement } from '../js/nelement/index.js'
+import { nElement, nLink } from '../js/nelement/index.js'
 import { TopMenu } from '../js/libs/menu/top.js'
-import { ParaVoceMenu } from '../js/libs/menu/para-voce.js'
 import { URLS } from '../js/utils/constants.js'
+import { ParaEmpresasMenu } from '../js/libs/menu/index.js'
 
 export class Page extends nElement {
   children = {
     top_menu: new TopMenu(URLS.PARAEMPRESAS),
-    main_menu: new ParaVoceMenu(),
+    main_menu: new ParaEmpresasMenu(),
   }
 
   onCreate() {
     this.setStyles()
     this.append(this.getTopMenu())
+    this.append(this.createLink(URLS.PARAEMPRESAS_))
     this.append(this.getMainMenu())
   }
 
@@ -21,6 +22,13 @@ export class Page extends nElement {
 
   getTopMenu() {
     return this.children.top_menu
+  }
+
+  createLink(href = '', text = '') {
+    const link = new nLink()
+    link.href(href)
+    link.setText(text)
+    return link
   }
 
   getMainMenu() {
