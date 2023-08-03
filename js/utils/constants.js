22831eac8e63b23dda368c80ce177fc92c35bdf9
Tarsis <br.tmvdl@gmail.com>
2023-08-03 07:30:47 -0300

    Adding constants file

---
+++ js/utils/constants.js
@@ -0,0 +1,17 @@
+const PARAVOCE = '/para-voce/'
+const PARAVOCE_ = '/para-voce/'
+const PARAEMPRESAS = '/para-empresas/'
+const PARADESENVOLVEDORES = '/para-desenvolvedores/'
+const MONTEOSEU = '/monte-o-seu/'
+const SOBDEMANDA = '/sob-demanda/'
+
+export default {
+  URLS: {
+    PARAVOCE,
+    PARAVOCE_,
+    PARAEMPRESAS,
+    PARADESENVOLVEDORES,
+    MONTEOSEU,
+    SOBDEMANDA,
+  }
+}
