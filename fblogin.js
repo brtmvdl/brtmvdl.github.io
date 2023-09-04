27fa54f458248b12ead7b558f2192f159c902221
Tarsis <br.tmvdl@gmail.com>
2023-09-04 11:53:56 -0300

    Updating "FB login state" files

--- fblogin.js
+++ fblogin.js
@@ -4,30 +4,34 @@ console.log({ app })
 
 const status = new Frontend()
 
-function statusChangeCallback(response) {
-  console.log('statusChangeCallback')
-  console.log(response)
-  if (response.status === 'connected') {
-    testAPI()
-  } else {
-    status.setText('Please log into this webpage.')
-  }
-}
-
-function checkLoginState() {
-  FB.getLoginStatus(function (response) {
-    statusChangeCallback(response)
-  })
-}
-
-function testAPI() {
-  console.log('Welcome!  Fetching your information.... ')
+function fb_api_me() {
+  console.log('Welcome! Fetching your information...')
   FB.api('/me', function (response) {
     console.log('Successful login for: ' + response.name)
     status.setText('Thanks for logging in, ' + response.name + '!')
   })
 }
+
+function fb_login() {
+  FB.login(function (response) { console.log('fb_login', { response }) })
+}
+
+function statusChangeCallback(response) {
+  console.log('statusChangeCallback', { response })
+  if (response.status === 'connected') {
+    fb_api_me()
+  } else if (response.status === 'not_authorized') {
+    fb_login()
+  } else {
+    status.setText('Please log into this webpage.')
+  }
+}
+
+function fb_get_login_status() {
+  FB.getLoginStatus(function (response) { statusChangeCallback(response) })
+}
+
 const btn = new nButton()
 btn.setText('FB login state')
-btn.on('click', () => checkLoginState())
+btn.on('click', () => fb_get_login_status())
 app.append(btn)
