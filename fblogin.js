3311ec638a586a8fa0d212233479a5cd2755611b
Tarsis <br.tmvdl@gmail.com>
2023-09-04 11:29:57 -0300

    Updating "FB login state" files

--- fblogin.js
+++ fblogin.js
@@ -2,30 +2,32 @@ import { Frontend, nButton } from '@brtmvdl/frontend'
 const app = Frontend.fromId('app')
 console.log({ app })
 
-const fb_app = {
-  log: (key, ...values) => fb_app.log('FB', key, ...values),
-  testAPI: () => {
-    fb_app.log('Welcome!  Fetching your information.... ')
-    FB.api('/me', (response) => {
-      fb_app.log('Successful login for: ' + response.name,)
-      fb_app.log('Thanks for logging in, ' + response.name + '!')
-    })
-  },
-  statusChangeCallback: (response) => {
-    fb_app.log('statusChangeCallback', { response })
-
-    if (response.status === 'connected') {
-      fb_app.testAPI()
-    } else {
-      fb_app.log('Please log into this webpage.')
-    }
-  },
-  checkLoginState: () => {
-    FB.getLoginStatus((response) => fb_app.statusChangeCallback(response))
-  },
+function statusChangeCallback(response) {
+  console.log('statusChangeCallback')
+  console.log(response)
+  if (response.status === 'connected') {
+    testAPI()
+  } else {
+    document.getElementById('status').innerHTML = 'Please log ' +
+      'into this webpage.'
+  }
 }
 
+function checkLoginState() {
+  FB.getLoginStatus(function (response) {
+    statusChangeCallback(response)
+  })
+}
+
+function testAPI() {
+  console.log('Welcome!  Fetching your information.... ')
+  FB.api('/me', function (response) {
+    console.log('Successful login for: ' + response.name)
+    document.getElementById('status').innerHTML =
+      'Thanks for logging in, ' + response.name + '!'
+  })
+}
 const btn = new nButton()
 btn.setText('FB login state')
-btn.on('click', () => fb_app.checkLoginState())
+btn.on('click', () => checkLoginState())
 app.append(btn)
