8ab62e5ca272a7128355660fecef8a0c4e28a849
Tarsis <br.tmvdl@gmail.com>
2023-09-08 17:12:10 -0300

    Updating experiences list file

--- libs/experiences.js
+++ libs/experiences.js
@@ -4,11 +4,19 @@ const TYPES = {
   EDUCATION: "education",
 }
 
+const npmPackages = [
+  'frontend',
+  'backend',
+  'database',
+  'logger',
+  'uuid',
+]
+
 export default [
   {
-    "title": "Piano (3D)",
-    "image": "/img/piano.png",
-    "link": "https://piano-brtmvdl.surge.sh/",
+    "title": "Calc",
+    "image": "/img/calc.png",
+    "link": "/projects/calc/",
   },
   {
     "title": "Clock (3D)",
@@ -16,85 +24,85 @@ export default [
     "link": "/projects/clock/",
   },
   {
+    "title": "Piano (3D)",
+    "image": "/img/piano.png",
+    "link": "https://piano-brtmvdl.surge.sh/",
+  },
+  {
     "title": "Nume",
     "image": "/img/nume.png",
     "link": "https://nume-brtmvdl.surge.sh/",
   },
-  {
-    "title": "Create nElement App",
-    "image": "/img/create-nelement-app.png",
-    "link": "https://www.npmjs.com/package/create-nelement-app",
-  },
+  ...npmPackages.map((pack) => ({
+    "title": `@brtmvdl/${pack}`,
+    "image": `/img/brtmvdl-${pack}.png`,
+    "link": `https://www.npmjs.com/package/@brtmvdl/${pack}`,
+  })),
   {
     "title": "Visual Studio Code Front-End Extension Pack",
     "image": "/img/frontend-extension-pack-vsce.png",
     "link": "https://marketplace.visualstudio.com/items?itemName=tmvdl.frontend-extension-pack-vsce",
   },
   {
-    "title": "Calc",
-    "image": "/img/calc.png",
-    "link": "/projects/calc/",
+    "title": "Youtube Download (Docker)",
+    "image": "/img/yt-dl.png",
+    "link": "https://hub.docker.com/r/tmvdl/yt-dl",
   },
-  // {
-  //   "title": "Youtube Download (Docker)",
-  //   "image": "/img/yt-dl.png",
-  //   "link": "https://hub.docker.com/r/tmvdl/yt-dl",
-  // },
-  // {
-  //   "title": "Alpine (Docker)",
-  //   "image": "/img/docker-alpine.png",
-  //   "link": "https://hub.docker.com/r/tmvdl/alpine",
-  // },
-  // {
-  //   "title": "Android (Docker)",
-  //   "image": "/img/docker-android.png",
-  //   "link": "https://hub.docker.com/r/tmvdl/android",
-  // },
-  // {
-  //   "title": "Dart (Docker)",
-  //   "image": "/img/docker-dart.png",
-  //   "link": "https://hub.docker.com/r/tmvdl/dart",
-  // },
-  // {
-  //   "title": ".Net (Docker)",
-  //   "image": "/img/docker-dotnet.png",
-  //   "link": "https://hub.docker.com/r/tmvdl/dotnet",
-  // },
-  // {
-  //   "title": "Go (Docker)",
-  //   "image": "/img/docker-go.png",
-  //   "link": "https://hub.docker.com/r/tmvdl/go",
-  // },
-  // {
-  //   "title": "Java (Docker)",
-  //   "image": "/img/docker-java.png",
-  //   "link": "https://hub.docker.com/r/tmvdl/java",
-  // },
-  // {
-  //   "title": "Kotlin (Docker)",
-  //   "image": "/img/docker-kotlin.png",
-  //   "link": "https://hub.docker.com/r/tmvdl/kotlin",
-  // },
-  // {
-  //   "title": "Node.js (Docker)",
-  //   "image": "/img/docker-node.png",
-  //   "link": "https://hub.docker.com/r/tmvdl/node",
-  // },
-  // {
-  //   "title": "PHP (Docker)",
-  //   "image": "/img/docker-php.png",
-  //   "link": "https://hub.docker.com/r/tmvdl/php",
-  // },
-  // {
-  //   "title": "Python (Docker)",
-  //   "image": "/img/docker-python.png",
-  //   "link": "https://hub.docker.com/r/tmvdl/python",
-  // },
-  // {
-  //   "title": "Ruby (Docker)",
-  //   "image": "/img/docker-ruby.png",
-  //   "link": "https://hub.docker.com/r/tmvdl/ruby",
-  // },
+  {
+    "title": "Alpine (Docker)",
+    "image": "/img/docker-alpine.png",
+    "link": "https://hub.docker.com/r/tmvdl/alpine",
+  },
+  {
+    "title": "Android (Docker)",
+    "image": "/img/docker-android.png",
+    "link": "https://hub.docker.com/r/tmvdl/android",
+  },
+  {
+    "title": "Dart (Docker)",
+    "image": "/img/docker-dart.png",
+    "link": "https://hub.docker.com/r/tmvdl/dart",
+  },
+  {
+    "title": ".Net (Docker)",
+    "image": "/img/docker-dotnet.png",
+    "link": "https://hub.docker.com/r/tmvdl/dotnet",
+  },
+  {
+    "title": "Go (Docker)",
+    "image": "/img/docker-go.png",
+    "link": "https://hub.docker.com/r/tmvdl/go",
+  },
+  {
+    "title": "Java (Docker)",
+    "image": "/img/docker-java.png",
+    "link": "https://hub.docker.com/r/tmvdl/java",
+  },
+  {
+    "title": "Kotlin (Docker)",
+    "image": "/img/docker-kotlin.png",
+    "link": "https://hub.docker.com/r/tmvdl/kotlin",
+  },
+  {
+    "title": "Node.js (Docker)",
+    "image": "/img/docker-node.png",
+    "link": "https://hub.docker.com/r/tmvdl/node",
+  },
+  {
+    "title": "PHP (Docker)",
+    "image": "/img/docker-php.png",
+    "link": "https://hub.docker.com/r/tmvdl/php",
+  },
+  {
+    "title": "Python (Docker)",
+    "image": "/img/docker-python.png",
+    "link": "https://hub.docker.com/r/tmvdl/python",
+  },
+  {
+    "title": "Ruby (Docker)",
+    "image": "/img/docker-ruby.png",
+    "link": "https://hub.docker.com/r/tmvdl/ruby",
+  },
   // {
   //   "title": "Chalkboard",
   //   "image": "/img/chalkboard.png",
@@ -274,10 +282,5 @@ export default [
   //   "title": "nelement",
   //   "image": "/img/nelement.png",
   //   "link": "https://nelement-brtmvdl.surge.sh/",
-  // },
-  // {
-  //   "title": "Calc (3D)",
-  //   "image": "/img/calc.png",
-  //   "link": "/projects/calc/",
   // },
 ]
