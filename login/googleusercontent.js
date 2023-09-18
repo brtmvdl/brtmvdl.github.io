8335b355daa25dee6e261969a21e2a4638a4d393
Tarsis <br.tmvdl@gmail.com>
2023-09-18 08:53:28 -0300

    Updating Google OAuth Login page file

--- login/googleusercontent.js
+++ login/googleusercontent.js
@@ -1,21 +1,21 @@
 const url = new URL(window.location)
 url.pathname = "login/google.html"
-url.hash = ''
+url.hash = ""
 
 const redirect_uri = url.toString()
 
 export const GOOGLE = {
   redirect_uri,
   "response_type": "token",
-  "scope": "https://www.googleapis.com/auth/userinfo.email",
-  "project_id": "apiv1-398721",
+  "scope": "https://www.googleapis.com/auth/spreadsheets",
+  "project_id": "brtmvdl",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "client_id": "635151893278-a7hdtv7ipfdvhpljhbvids5cnc70ebrv.apps.googleusercontent.com",
+  "client_id": "387615697147-ae61ofb1tnqaks4a9repv15bf54mpqp7.apps.googleusercontent.com",
   "redirect_uris": [
     "https://brtmvdl.surge.sh/login/google.html",
     "http://127.0.0.1:8080/login/google.html",
-    "http://brtmvdl.surge.sh",
+    "https://brtmvdl.surge.sh",
     "http://127.0.0.1:8080",
   ],
   "javascript_origins": [
