cc2a30698e24c304f03cc6a90d7dd8994dbff127
Tarsis <br.tmvdl@gmail.com>
2023-10-24 11:30:12 -0300

    Updating some pages files

---
+++ privacy-policy.js
@@ -0,0 +1,7 @@
+import { nElement, nFlex, nH1 } from './js/nelement/index.js'
+
+export class Page extends nElement {
+  onCreate() {
+    this.setText('privacy policy')
+  }
+}
---
+++ terms-of-service.js
@@ -0,0 +1,7 @@
+import { nElement } from './js/nelement/index.js'
+
+export class Page extends nElement {
+  onCreate() {
+    this.setText('terms of service')
+  }
+}
