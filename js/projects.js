a7526436879fbd5385b1daa1cfb2c6d2eac818d4
Tarsis <br.tmvdl@gmail.com>
2023-10-25 10:52:48 -0300

    Updating projects list file

--- js/projects.js
+++ js/projects.js
@@ -1,39 +1,182 @@
-
 export const projects = [
-  "projeto",
-  "pomodoro",
-  "piano",
-  "justeat",
-  "json",
-  "jornal",
-  "jobssender",
-  "games",
-  "curriculus",
-  "clock",
-  "chess",
-  "chalkboard",
-  "cento",
-  "calendar",
-  "metronomo",
-  "market",
-  "mapping",
-  "study",
-  "stroll",
-  "school",
-  "youchat",
-  "debian",
-  "drawer",
-  "nume",
-  "numbers",
-  "ncontext",
-  "namazon",
-  "nelement",
-  "ball",
-  "airplane",
-  "apply",
-  "app",
-  "vestibular",
-  "raining",
-  "hours",
-  "qrcodescanner",
+  {
+    "title": "airplane",
+    "subtitle": "",
+    "url": "airplane-brtmvdl.surge.sh"
+  },
+  {
+    "title": "brtmvdl",
+    "subtitle": "",
+    "url": "brtmvdl.surge.sh"
+  },
+  {
+    "title": "cube",
+    "subtitle": "",
+    "url": "cube-brtmvdl.surge.sh"
+  },
+  {
+    "title": "coins",
+    "subtitle": "",
+    "url": "coins-brtmvdl.surge.sh"
+  },
+  {
+    "title": "edsonjunior-22",
+    "subtitle": "",
+    "url": "edsonjunior-22.surge.sh"
+  },
+  {
+    "title": "debian",
+    "subtitle": "",
+    "url": "debian-brtmvdl.surge.sh"
+  },
+  {
+    "title": "home-workout",
+    "subtitle": "",
+    "url": "home-workout-brtmvdl.surge.sh"
+  },
+  {
+    "title": "math",
+    "subtitle": "",
+    "url": "math-brtmvdl.surge.sh"
+  },
+  {
+    "title": "metronomo",
+    "subtitle": "",
+    "url": "metronomo-brtmvdl.surge.sh"
+  },
+  {
+    "title": "market",
+    "subtitle": "",
+    "url": "market-brtmvdl.surge.sh"
+  },
+  {
+    "title": "nume",
+    "subtitle": "",
+    "url": "nume-brtmvdl.surge.sh"
+  },
+  {
+    "title": "numbers",
+    "subtitle": "",
+    "url": "numbers-brtmvdl.surge.sh"
+  },
+  {
+    "title": "ncontext",
+    "subtitle": "",
+    "url": "ncontext-brtmvdl.surge.sh"
+  },
+  {
+    "title": "pomodoro",
+    "subtitle": "",
+    "url": "pomodoro-brtmvdl.surge.sh"
+  },
+  {
+    "title": "piano",
+    "subtitle": "",
+    "url": "piano-brtmvdl.surge.sh"
+  },
+  {
+    "title": "study",
+    "subtitle": "",
+    "url": "study-brtmvdl.surge.sh"
+  },
+  {
+    "title": "stroll",
+    "subtitle": "",
+    "url": "stroll-brtmvdl.surge.sh"
+  },
+  {
+    "title": "school",
+    "subtitle": "",
+    "url": "school-brtmvdl.surge.sh"
+  },
+  {
+    "title": "youchat",
+    "subtitle": "",
+    "url": "youchat-brtmvdl.surge.sh"
+  },
+  {
+    "title": "vestibular",
+    "subtitle": "",
+    "url": "vestibular-brtmvdl.surge.sh"
+  },
+  {
+    "title": "justeat",
+    "subtitle": "",
+    "url": "justeat-brtmvdl.surge.sh"
+  },
+  {
+    "title": "jornal",
+    "subtitle": "",
+    "url": "jornal-brtmvdl.surge.sh"
+  },
+  {
+    "title": "hours",
+    "subtitle": "",
+    "url": "hours-brtmvdl.surge.sh"
+  },
+  {
+    "title": "games",
+    "subtitle": "",
+    "url": "games-brtmvdl.surge.sh"
+  },
+  {
+    "title": "drawer",
+    "subtitle": "",
+    "url": "drawer-brtmvdl.surge.sh"
+  },
+  {
+    "title": "curriculus",
+    "subtitle": "",
+    "url": "curriculus-brtmvdl.surge.sh"
+  },
+  {
+    "title": "clock",
+    "subtitle": "",
+    "url": "clock-brtmvdl.surge.sh"
+  },
+  {
+    "title": "chess",
+    "subtitle": "",
+    "url": "chess-brtmvdl.surge.sh"
+  },
+  {
+    "title": "cento",
+    "subtitle": "",
+    "url": "cento-brtmvdl.surge.sh"
+  },
+  {
+    "title": "calendar",
+    "subtitle": "",
+    "url": "calendar-brtmvdl.surge.sh"
+  },
+  {
+    "title": "apply",
+    "subtitle": "",
+    "url": "apply-brtmvdl.surge.sh"
+  },
+  {
+    "title": "app",
+    "subtitle": "",
+    "url": "app-brtmvdl.surge.sh"
+  },
+  {
+    "title": "namazon",
+    "subtitle": "",
+    "url": "namazon-brtmvdl.surge.sh"
+  },
+  {
+    "title": "qrcodescanner",
+    "subtitle": "",
+    "url": "qrcodescanner-brtmvdl.surge.sh"
+  },
+  {
+    "title": "nelement",
+    "subtitle": "",
+    "url": "nelement-brtmvdl.surge.sh"
+  },
+  {
+    "title": "brtmvdl-calendario",
+    "subtitle": "",
+    "url": "brtmvdl-calendario.surge.sh"
+  }
 ]
