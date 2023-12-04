df264b7f177952f9ee8d1b2be1c2bf9f64aad4c3
Tarsis <br.tmvdl@gmail.com>
2023-12-04 12:28:04 -0300

    Updating app files

--- login/youtube/config.js
+++ login/youtube/config.js
@@ -1,5 +1,3 @@
-import * as GoogleUserContent from './googleusercontent.js'
+// 
 
-export const API_KEY = GoogleUserContent.default.web.client_secret
-
-export const CLIENT_ID = GoogleUserContent.default.web.client_id
+export const API_KEY = ''
