f133275e34076b79c44181f24939caf15a494ec6
Tarsis <br.tmvdl@gmail.com>
2023-12-03 21:22:33 -0300

    Updating app files

--- login/youtube/README.md
+++ login/youtube/README.md
@@ -1,3 +1,5 @@
 # YouTube - Live Streaming API
 
 [LiveBroadcasts: list](https://developers.google.com/youtube/v3/live/docs/liveBroadcasts/list)
+
+[create Youtube Stream - example](https://github.com/toshvelaga/livestream/blob/eb6f05417a0db5cf4c68f8835daedd1fc61be51e/server/utils/createYoutubeStream.js#L33)
--- login/youtube/index.js
+++ login/youtube/index.js
@@ -1,33 +1,52 @@
 import { HTML, nButton } from '@brtmvdl/frontend'
 
-import { API_KEY } from './config.js'
-
 import * as GoogleUserContent from './googleusercontent.js'
 
-export class Page extends HTML {
-  children = {
-    login_button: new nButton()
-  }
+const scope = 'https://www.googleapis.com/auth/youtube.force-ssl'
 
-  onCreate() {
-    this.append(this.getLoginButton())
+const client_id = GoogleUserContent.default.web.client_id
 
-    this.initGoogleClient()
-  }
+const client_secret = GoogleUserContent.default.web.client_secret
 
-  getLoginButton() {
-    this.children.login_button.setText('login')
+const scheduledStartTime = ''
 
-    this.children.login_button.on('click', () => console.log('login'))
+const title = 'Pomodoro test 1'
 
-    return this.children.login_button
-  }
+const privacyStatus = ''
 
-  initGoogleClient() {
-    const apiKey = GoogleUserContent.default.web.client_secret
-
-    gapi.client.init({ apiKey })
-      .then((res) => console.log('res', res))
-      .catch((err) => console.log('err', err))
-  }
+function authenticate() {
+  return gapi.auth2.getAuthInstance().signIn({ scope })
+    .then(() => console.log('Sign-in successful'))
+    .catch(err => console.error('Error signing in', err))
 }
+
+function loadClient() {
+  gapi.client.setApiKey(client_secret)
+
+  return gapi.client.load('https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest')
+    .then(() => console.log('GAPI client loaded for API'))
+    .catch((err) => console.error('Error loading GAPI client for API', err))
+}
+
+function execute() {
+  return gapi.client.youtube.liveBroadcasts.insert({
+    'resource': {
+      'contentDetails': { },
+      'snippet': { title, scheduledStartTime, privacyStatus }
+    }
+  })
+    .then((res) => console.log('Response', res))
+    .catch((err) => console.error('Execute error', err))
+}
+
+gapi.load('client:auth2', () => gapi.auth2.init({ client_id }))
+
+//
+
+const authenticateButton = new nButton()
+authenticateButton.setText('authorize and load')
+authenticateButton.on('click', () => authenticate().then(loadClient))
+
+const executeButton = new nButton()
+executeButton.setText('execute')
+executeButton.on('click', () => execute())
