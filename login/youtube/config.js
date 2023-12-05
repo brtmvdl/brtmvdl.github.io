9d85d61d4ac6e6c0f2f8f2a939f353e88c43ac81
Tarsis <br.tmvdl@gmail.com>
2023-12-05 18:02:06 -0300

    Updating app files

--- login/youtube/config.js
+++ login/youtube/config.js
@@ -1,3 +1,7 @@
 // 
 
+export const CLIENT_ID = ''
+
 export const API_KEY = ''
+
+export const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest']
--- login/youtube/index.js
+++ login/youtube/index.js
@@ -1,15 +1,37 @@
-import { API_KEY } from './config.js'
+import { HTML, nButton, nInputTextGroup } from '@brtmvdl/frontend'
 
-const initConfig = {
-  'apiKey': API_KEY,
-  'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'],
-}
+import { API_KEY, DISCOVERY_DOCS, CLIENT_ID } from './config.js'
 
-const listChannels = () =>
-  gapi.client.youtube.channels
-    .list({ part: 'statistics', forUsername: 'brtmvdl' })
-    .execute((res) => console.log('youtube.channels', res))
+const listChannels = (forUsername, part = 'statistics') => new Promise((s) =>
+  gapi.client.youtube.channels.list({ part, forUsername }).execute(s)
+)
 
-const start = () => gapi.client.init(initConfig).then(listChannels)
+const start = () => gapi.client.init({ 'apiKey': API_KEY, 'discoveryDocs': DISCOVERY_DOCS })
 
-gapi.load('client', start)
+const gapiLoad = () => new Promise((s) => gapi.load('client', s))
+
+// //
+
+const app = HTML.fromId('app')
+
+const usernameInput = new nInputTextGroup()
+usernameInput.children.label.setText('username')
+app.append(usernameInput)
+
+const listChannelsButton = new nButton()
+listChannelsButton.setText('list channels')
+listChannelsButton.on('click', () => {
+  listChannels(usernameInput.children.input.getValue())
+    .then(console.log).catch(console.error)
+})
+app.append(listChannelsButton)
+
+// //
+
+gapiLoad().then(() => start()).then((res) => console.log('res', res))
+
+const loginButton = new nButton()
+loginButton.setText('login')
+loginButton.on('click', () => gapi.auth2.authorize({ client_id: CLIENT_ID }).execute(console.log))
+
+gapi.auth2.authorize({ scope: 'youtube', client_id: CLIENT_ID }, console.log)
