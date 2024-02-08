b089c9fb4ec376ba57ccbe8564cf4bdebb513017
Tarsis <br.tmvdl@gmail.com>
2024-02-08 14:26:44 -0300

    Updating app files

--- xp/calc/index.js
+++ xp/calc/index.js
@@ -1,39 +1,25 @@
 import { HTML } from '@brtmvdl/frontend'
-import { HeaderHTML } from './components/header.html.js'
-import { BodyHTML } from './components/body.html.js'
-import { FooterHTML } from './components/footer.html.js'
+import { Head } from './components/head.js'
+import { Body } from './components/body.js'
 
 export class Page extends HTML {
   children = {
-    header: new HeaderHTML(),
-    body: new BodyHTML(),
-    footer: new FooterHTML(),
+    header: new Head(),
+    body: new Body(),
   }
 
   onCreate() {
-    super.onCreate()
-    this.setStyles()
     this.append(this.getHeader())
     this.append(this.getBody())
-    this.append(this.getFooter())
-  }
-
-  setStyles() {
-    this.setStyle('margin', '0 auto')
-    this.setStyle('width', '40rem')
   }
 
   getHeader() {
     this.children.header.on('createproject', () => this.children.body.dispatchEvent('createproject'))
+
     return this.children.header
   }
 
   getBody() {
-    this.children.body.on('update', () => this.children.footer.dispatchEvent('update'))
     return this.children.body
-  }
-
-  getFooter() {
-    return this.children.footer
   }
 }
