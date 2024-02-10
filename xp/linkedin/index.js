38ac210605328693ca1f49f14da7a829a920b379
Tarsis <br.tmvdl@gmail.com>
2024-02-10 12:04:04 -0300

    Updating app files

--- xp/linkedin/index.js
+++ xp/linkedin/index.js
@@ -1,13 +1,9 @@
 import { HTML, nButton } from '@brtmvdl/frontend'
-
 import * as API from './api.js'
 
-const app = HTML.fromId('app')
-
-const title = new HTML()
-title.setText('Share on LinkedIn API')
-app.append(title)
-
-const button = new nButton()
-app.append(button.setText('userinfo'))
-button.on('click', () => API.userinfo().then(console.log).catch(console.error))
+export class Page extends HTML {
+  onCreate() {
+    super.onCreate()
+    this.setText('Share on LinkedIn API')
+  }
+}
