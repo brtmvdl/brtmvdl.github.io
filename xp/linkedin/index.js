febe6b0586139739c7921e56cbf1c3bfac4c00d7
Tarsis <br.tmvdl@gmail.com>
2024-02-10 12:15:23 -0300

    Updating app files

--- xp/linkedin/index.js
+++ xp/linkedin/index.js
@@ -1,9 +1,27 @@
-import { HTML, nButton } from '@brtmvdl/frontend'
-import * as API from './api.js'
+import { HTML, nLink } from '@brtmvdl/frontend'
 
 export class Page extends HTML {
   onCreate() {
     super.onCreate()
-    this.setText('Share on LinkedIn API')
+    this.append(this.getTitle())
+    this.append(this.getAuthorizationLink())
+  }
+
+  getTitle() {
+    const title = new nH2()
+    return title.setText('Share on LinkedIn API')
+  }
+
+  getAuthorizationLink() {
+    const nlink = new nLink()
+    const response_type = 'code'
+    const client_id = config.apiKey
+    const redirect_uri = 'https://brtmvdl.github.io/xp/linkedin/'
+    const state = ''
+    const scope = encodeURIComponent(['liteprofile', 'emailaddress'].join(' '))
+    const link = `https://www.linkedin.com/oauth/v2/authorization?response_type=${response_type}&client_id=${client_id}&redirect_uri=${redirect_uri}&state=${state}&scope=${scope}`
+    nlink.href(link)
+    nlink.setText('authorization')
+    return nlink
   }
 }
