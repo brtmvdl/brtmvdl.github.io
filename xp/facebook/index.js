8325c04e0a9d3a344bfbf529328775a9c71d1164
Tarsis <br.tmvdl@gmail.com>
2024-02-10 10:14:15 -0300

    Updating app files

--- xp/facebook/index.js
+++ xp/facebook/index.js
@@ -1,15 +1,31 @@
 import { HTML, nButton } from '@brtmvdl/frontend'
 
+class TextHTML extends HTML {
+  text = null
+
+  constructor(text = '') {
+    super()
+    this.text = text
+  }
+
+  onCreate() {
+    super.onCreate()
+    this.setText(this.text)
+  }
+}
+
 export class Page extends HTML {
   children = {
     title: new HTML(),
     login_status_button: new nButton(),
     login_button: new nButton(),
+    responses: new HTML(),
   }
 
   onCreate() {
     this.append(this.getLoginStatusButton())
     this.append(this.getLoginButton())
+    this.append(this.getResponses())
   }
 
   getTitle() {
@@ -19,13 +35,17 @@ export class Page extends HTML {
 
   getLoginStatusButton() {
     this.children.login_status_button.setText('Get Login Status')
-    this.children.login_status_button.on('click', () => FB.getLoginStatus((resp) => console.log('FB.getLoginStatus', { resp })))
+    this.children.login_status_button.on('click', () => FB.getLoginStatus((resp) => this.children.responses.append(new TextHTML(JSON.stringify({ name: 'Get Login Status', resp })))))
     return this.children.login_status_button
   }
 
   getLoginButton() {
     this.children.login_button.setText('Login')
-    this.children.login_button.on('click', () => FB.login((resp) => console.log('FB.login', { resp })))
+    this.children.login_button.on('click', () => FB.login((resp) => this.children.responses.append(new TextHTML(JSON.stringify({ name: 'Login', resp })))))
     return this.children.login_button
+  }
+
+  getResponses() {
+    return this.children.responses
   }
 }
