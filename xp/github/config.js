de3a0a13ecc8740882a4c4949da96eb3f8868426
Tarsis <br.tmvdl@gmail.com>
2024-02-14 11:38:05 -0300

    Updating app files

--- xp/github/config.js
+++ xp/github/config.js
@@ -1,5 +1,5 @@
-// 
+//
 
 export const client_id = '8f5b4e9b495ccb7b01e4'
 
-export const access_token = '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'
+export const access_token = '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'
