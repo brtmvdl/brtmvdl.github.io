2f46facfd2808ee0575a9cf71a1912d1857bb7d5
Tarsis <br.tmvdl@gmail.com>
2024-02-14 10:51:57 -0300

    Updating app files

--- xp/github/config.js
+++ xp/github/config.js
@@ -1,3 +1,5 @@
 // 
 
 export const client_id = '8f5b4e9b495ccb7b01e4'
+
+export const access_token = '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'
--- xp/github/index.js
+++ xp/github/index.js
@@ -1,13 +1,33 @@
 import { HTML, nLink } from '@brtmvdl/frontend'
-import { client_id } from './config.js'
+import { client_id, access_token } from './config.js'
 import * as Local from '../../assets/js/utils/local.js'
 import * as Flow from '../../assets/js/utils/flow.js'
 
+class TextHTML extends HTML {
+  text = null
+
+  constructor(text = '') {
+    super()
+    this.text = text
+  }
+
+  onCreate() {
+    super.onCreate()
+    this.setText(this.text)
+  }
+}
+
 export class Page extends HTML {
+  children = {
+    responses: new HTML(),
+  }
+
   onCreate() {
     super.onCreate()
     this.append(this.getTitleHTML())
     this.append(this.getLoginLink())
+    this.append(this.getApiUserButton())
+    this.append(this.getResonsesHTML())
     this.setOauthCode()
   }
 
@@ -22,6 +42,24 @@ export class Page extends HTML {
     link.setText('login')
     link.href(`https://github.com/login/oauth/authorize?scope=user:email&client_id=${client_id}`)
     return link
+  }
+
+  getApiUserButton() {
+    const button = new nButton()
+    button.setText('api.github.com/user') // await 
+    button.on('click', () => this.onApiUserButton())
+    return button
+  }
+
+  onApiUserButton() {
+    fetch('https://api.github.com/user', { headers })
+      .then(res => res.json())
+      .then((json) => this.children.responses.append(new TextHTML(JSON.stringify(json))))
+      .catch((err) => this.children.responses.append(new TextHTML(err.message)))
+  }
+
+  getResonsesHTML() {
+    return this.children.responses
   }
 
   setOauthCode() {
