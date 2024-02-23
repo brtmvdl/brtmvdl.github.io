20c5035b9ec089b16cdaa665d78ddeb4c5286b7c
Tarsis <br.tmvdl@gmail.com>
2024-02-23 16:55:12 -0300

    Updating app files

---
+++ xp/bitget/components/header.component.js
@@ -0,0 +1,51 @@
+import { HTML, nFlex, nLink, nImage } from '@brtmvdl/frontend'
+import { ip } from '../../../assets/js/utils/net.js'
+
+export class HeaderComponent extends HTML {
+  children = {
+    ip: new HTML(),
+  }
+
+  onCreate() {
+    super.onCreate()
+    this.append(this.getFlex())
+    this.getIp()
+  }
+
+  getFlex() {
+    const flex = new nFlex()
+    flex.append(this.getLeft())
+    flex.append(this.getRight())
+    return flex
+  }
+
+  getLeft() {
+    const left = new HTML()
+    left.append(this.getLogoImageLink())
+    return left
+  }
+
+  getLogoImageLink() {
+    const link = new nLink()
+    link.href('https://www.bitget.com/api-doc/common/websocket-intro')
+    const image = new nImage()
+    image.src('./logo.svg')
+    link.append(image)
+    return link
+  }
+
+  getRight() {
+    const right = new HTML()
+    right.append(this.getIpHTML())
+    return right
+  }
+
+  getIpHTML() {
+    this.children.ip.setStyle('padding', '1rem')
+    return this.children.ip
+  }
+
+  getIp() {
+    ip().then((res) => this.children.ip.setText(res.ip))
+  }
+}
