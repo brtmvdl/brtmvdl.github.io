a11470a3456834567301e6697a9c694b1e36ced0
Tarsis <br.tmvdl@gmail.com>
2024-02-23 16:58:04 -0300

    Updating app files

--- xp/bitget/components/content.component.js
+++ xp/bitget/components/content.component.js
@@ -1,3 +1,27 @@
-import { HTML } from '@brtmvdl/frontend'
+import { HTML, nFlex } from '@brtmvdl/frontend'
 
-export class ContentComponent extends HTML { }
+export class ContentComponent extends HTML {
+  onCreate() {
+    super.onCreate()
+    this.append(this.getFlex())
+  }
+
+  getFlex() {
+    const flex = new nFlex()
+    flex.append(this.getLeft())
+    flex.append(this.getRight())
+    return flex
+  }
+
+  getLeft() {
+    const left = new HTML()
+    left.setText('left')
+    return left
+  }
+
+  getRight() {
+    const right = new HTML()
+    right.setText('right')
+    return right
+  }
+}
