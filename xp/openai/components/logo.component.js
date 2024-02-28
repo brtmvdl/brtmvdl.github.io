8e9d4053d7606ab70cd27429622a318adc9376ba
Tarsis <br.tmvdl@gmail.com>
2024-02-28 11:52:12 -0300

    Updating app files

--- xp/openai/components/logo.component.js
+++ xp/openai/components/logo.component.js
@@ -1,9 +1,17 @@
-import { HTML, nImage } from '@brtmvdl/frontend'
+import { HTML, nLink, nImage } from '@brtmvdl/frontend'
 
-export class LogoComponent extends nImage {
+export class LogoComponent extends HTML {
   onCreate() {
     super.onCreate()
-    this.src('./logo.svg')
+    this.append(this.getImageLink())
     this.setStyle('max-height', '3rem')
+  }
+
+  getImageLink() {
+    const link = new nLink()
+    link.setContainerStyle('max-width', '100%')
+    link.href('https://platform.openai.com/docs/api-reference')
+    link.setText('OpenAI')
+    return link
   }
 }
