eba6e91a1c304c3f3df359a83f42d94de196edbb
Tarsis <br.tmvdl@gmail.com>
2024-03-18 19:14:48 -0300

    Updating app files

---
+++ xp/foxbit-websocket-api/utils/config.js
@@ -0,0 +1,9 @@
+// 
+
+export const apiKey = ''
+
+export const secretKey = ''
+
+export const url = 'wss://ws-api.binance.com/ws-api/v3'
+
+// export const url = 'wss://testnet.binance.vision/ws-api/v3'
