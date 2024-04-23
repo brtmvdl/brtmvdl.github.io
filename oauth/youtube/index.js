a3e23393adccecce231ab575bc721ad869ee90a6
Tarsis <br.tmvdl@gmail.com>
2024-04-23 11:54:37 -0300

    Updating app files

---
+++ oauth/youtube/index.js
@@ -0,0 +1,11 @@
+// 
+
+const searchParams = new URLSearchParams(window.location.hash.substr('1'))
+
+const access_token = searchParams.get('access_token')
+
+console.log({ access_token })
+
+localStorage.setItem('youtube.token', access_token)
+
+document.write(`access_token: ${access_token}`)
