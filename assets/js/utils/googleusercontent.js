79bbd1ebcda0f739666ce18a9f7d94bdfd787060
Tarsis <br.tmvdl@gmail.com>
2024-04-23 12:12:03 -0300

    Updating app files

--- assets/js/utils/googleusercontent.js
+++ assets/js/utils/googleusercontent.js
@@ -1,25 +1,33 @@
-const redirect_uri = 'https://brtmvdl.github.io/oauth/youtube/' // window.location.toString()
-
 export const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'
 
 export const API_KEY = ''
 
-export const GOOGLE = {
-  redirect_uri,
-  'response_type': 'token',
-  'scope': 'https://www.googleapis.com/auth/spreadsheets',
-  'project_id': 'brtmvdl',
-  'token_uri': 'https://oauth2.googleapis.com/token',
-  'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
-  'client_id': '387615697147-eefdeuk5mgaadcrnap1c4dl7j1rqn0sg.apps.googleusercontent.com',
-  // 'client_secret': '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',
-  'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
-  'redirect_uris': [
-    'https://brtmvdl.github.io/pages/login/',
-    'http://localhost:8000/pages/login/'
-  ],
-  'javascript_origins': [
-    'https://brtmvdl.github.io',
-    'http://localhost:8080'
-  ]
-}
+export const response_type = 'token'
+
+export const scope = 'https://www.googleapis.com/auth/spreadsheets'
+
+export const project_id = 'brtmvdl'
+
+export const token_uri = 'https://oauth2.googleapis.com/token'
+
+export const auth_uri = 'https://accounts.google.com/o/oauth2/auth'
+
+export const client_id = '387615697147-eefdeuk5mgaadcrnap1c4dl7j1rqn0sg.apps.googleusercontent.com'
+
+export const client_secret = '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'
+
+export const auth_provider_x509_cert_url = 'https://www.googleapis.com/oauth2/v1/certs'
+
+export const redirect_uris = [
+  'https://brtmvdl.github.io/login/',
+  'http://localhost:8000/login/'
+]
+
+export const javascript_origins = [
+  'https://brtmvdl.github.io',
+  'http://localhost:8080'
+]
+
+export const redirect_uri = redirect_uris.find(uri => uri.indexOf(window.location.hostname) != -1)
+
+export const GOOGLE = { response_type, scope, project_id, token_uri, auth_uri, client_id, auth_provider_x509_cert_url, redirect_uri, javascript_origins }
