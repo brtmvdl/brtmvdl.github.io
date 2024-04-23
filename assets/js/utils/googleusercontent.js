85f3973f7c33dc29c0ddbe77ccd00fed62f93c21
Tarsis <br.tmvdl@gmail.com>
2024-04-23 11:39:18 -0300

    Updating app files

--- assets/js/utils/googleusercontent.js
+++ assets/js/utils/googleusercontent.js
@@ -11,15 +11,17 @@ export const GOOGLE = {
   "project_id": "brtmvdl",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "client_id": "387615697147-r4t617erjqspc36a7s8tvudj25ni87uv.apps.googleusercontent.com",
-  // "client_secret": "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",
+  "client_id": "387615697147-eefdeuk5mgaadcrnap1c4dl7j1rqn0sg.apps.googleusercontent.com",
+  // "client_secret": "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",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
   "redirect_uris": [
-    "https://brtmvdl.github.io/xp/google/",
-    "http://localhost:8080/xp/google/"
+    "https://brtmvdl.github.io/pages/login/",
+    "https://brtmvdl.github.io/login/",
+    "http://localhost:8000/pages/login/",
+    "http://localhost:8080/login/"
   ],
   "javascript_origins": [
     "https://brtmvdl.github.io",
     "http://localhost:8080"
   ]
-} 
+}
