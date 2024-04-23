dfa99e1e896ccd9bdab7fa5e798574d7ec87a3cc
Tarsis <br.tmvdl@gmail.com>
2024-04-23 11:44:49 -0300

    Updating app files

--- assets/js/utils/googleusercontent.js
+++ assets/js/utils/googleusercontent.js
@@ -1,4 +1,4 @@
-const redirect_uri = window.location.toString()
+const redirect_uri =  'https://brtmvdl.github.io/login/' // window.location.toString()
 
 export const DISCOVERY_DOC = "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"
 
