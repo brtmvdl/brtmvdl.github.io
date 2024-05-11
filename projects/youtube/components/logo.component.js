8d55ae655e5cee7fecb71eaf5c23996605abeb13
Tarsis <br.tmvdl@gmail.com>
2024-05-11 18:26:22 -0300

    Updating app files

--- projects/youtube/components/logo.component.js
+++ projects/youtube/components/logo.component.js
@@ -1,9 +1,27 @@
-import { HTML, nImage } from '@brtmvdl/frontend'
+import { HTML, nLink, nImage } from '@brtmvdl/frontend'
 
-export class LogoComponent extends nImage {
+export class LogoComponent extends HTML {
   onCreate() {
     super.onCreate()
-    this.src('./logo.png')
+    this.setStyles()
+    this.append(this.getLogoLink())
+  }
+
+  setStyles() {
     this.setStyle('max-height', '3rem')
+  }
+
+  getLogoLink() {
+    const link = new nLink()
+    link.href('/')
+    link.append(this.getImage())
+    return link
+  }
+
+  getImage() {
+    const image = new nImage()
+    image.src('./logo.png')
+    image.setAttt('logo')
+    return image
   }
 }
