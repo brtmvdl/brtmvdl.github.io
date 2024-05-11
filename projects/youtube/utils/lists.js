7c4332f0e9c649e71371112fd1b49c63a60448ae
Tarsis <br.tmvdl@gmail.com>
2024-05-11 19:54:04 -0300

    Updating app files

--- projects/youtube/utils/lists.js
+++ projects/youtube/utils/lists.js
@@ -1,7 +1,7 @@
 import { RequestModel } from '../models/request.model.js'
 
-export const getMethodsList = () => Array.from([
+export const getRequestList = () => Array.from([
   new RequestModel('Youtube API', null, '/'),
-  new RequestModel('Videos: list ', 'GET', '/videos', { query: ['id'] }),
-  new RequestModel('Videos: insert', 'POST', '/videos'),
+  new RequestModel('Videos: list', 'GET', '/youtube/v3/videos', { query: ['part', 'chart'] }),
+  new RequestModel('Videos: insert', 'POST', '/upload/youtube/v3/videos'),
 ])
