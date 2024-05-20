12694deeedd5276e213ab1a5b9b371b52328dcd6
Tarsis <br.tmvdl@gmail.com>
2024-05-20 17:10:34 -0300

    Updating app files

--- projects/newsapi/components/header.component.js
+++ projects/newsapi/components/header.component.js
@@ -1,3 +1,25 @@
 import { HTML } from '@brtmvdl/frontend'
 
-export class HeaderComponent extends HTML { }
+export class HeaderComponent extends HTML {
+  state = {
+    title: '',
+  }
+
+  constructor(title = '') {
+    super()
+    this.state.title = title
+  }
+
+  onCreate() {
+    super.onCreate()
+    this.append(this.getTitleHTML())
+  }
+
+  getTitleHTML() {
+    const html = new HTML()
+    html.setText(this.state.title)
+    html.setStyle('padding', '1rem')
+    html.setStyle('font-size', '2rem')
+    return html
+  }
+}
--- projects/newsapi/index.js
+++ projects/newsapi/index.js
@@ -1,31 +1,9 @@
 import { HTML } from '@brtmvdl/frontend'
 import { HeaderComponent } from './components/header.component.js'
-import { FooterComponent } from './components/footer.component.js'
 
 export class Page extends HTML {
-  children = {
-    header: new HeaderComponent(),
-    content: new HTML(),
-    footer: new FooterComponent(),
-  }
-
   onCreate() {
     super.onCreate()
-    this.append(this.getHeaderHTML())
-    this.append(this.getContentHTML())
-    this.append(this.getFooterHTML())
+    this.append(new HeaderComponent('News API'))
   }
-
-  getHeaderHTML() {
-    return this.children.header
-  }
-
-  getContentHTML() {
-    return this.children.content
-  }
-
-  getFooterHTML() {
-    return this.children.footer
-  }
-
 }
