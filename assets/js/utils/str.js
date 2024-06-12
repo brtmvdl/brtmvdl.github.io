e3c15da18e8379d27db2bd544092003347d42ccc
Tarsis <br.tmvdl@gmail.com>
2024-06-12 13:44:33 -0300

    Updating app files

--- assets/js/utils/str.js
+++ assets/js/utils/str.js
@@ -1,4 +1,3 @@
-// 
 
 export const padLeft = (text = '', length = 1, pad = ' ') => {
   while (text.toString().length < length) text = pad.toString() + text.toString()
@@ -24,4 +23,9 @@ export const secondsToMinutes = (s = 0) => {
   const hours = (s * (60 ** 2)) - (seconds * (60)) - (minutes * (60 * 60))
 
   return `${padLeft(hours, 2, '0')}:${padLeft(minutes, 2, '0')}:${padLeft(seconds, 2, '0')}`
+}
+
+export const timestamp2str = (timestamp = Date.now()) => {
+  const date = new Date(timestamp)
+  return `${date.getFullYear()}/${padLeft(date.getMonth() + 1, 2, '0')}/${padLeft(date.getDate(), 2, '0')} ${padLeft(date.getHours(), 2, '0')}:${padLeft(date.getMinutes(), 2, '0')}:${padLeft(date.getSeconds(), 2, '0')}`
 }
--- projects/twitch-websocket-api/utils/str.js
+++
@@ -1,5 +0,0 @@
-
-export const timestamp2str = (timestamp = Date.now()) => {
-  const date = new Date(timestamp)
-  return `${date.getFullYear()}/${padLeft(date.getMonth() + 1, 2, '0')}/${padLeft(date.getDate(), 2, '0')} ${padLeft(date.getHours(), 2, '0')}:${padLeft(date.getMinutes(), 2, '0')}:${padLeft(date.getSeconds(), 2, '0')}`
-}
