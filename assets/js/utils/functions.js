39f4f0929128df8c7feb8806eeabf3e030599afb
Tarsis <br.tmvdl@gmail.com>
2024-06-18 11:46:20 -0300

    Updating app files

--- assets/js/utils/functions.js
+++ assets/js/utils/functions.js
@@ -1,3 +1,5 @@
 //
 
 export const uuid = () => Date.now().toString() + (Math.floor((10 ** 20 * Math.random()).toString().length))
+
+export const qrcode = (data = window.location.toString(), size = '150x150') => `https://api.qrserver.com/v1/create-qr-code/?size=${size}&data=${data}`
