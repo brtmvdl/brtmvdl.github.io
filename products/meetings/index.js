ce8fb99904794ff24d6b618f802a1d43a9e75aed
Tarsis <br.tmvdl@gmail.com>
2024-06-23 17:27:55 -0300

    Updating app files

--- products/meetings/index.js
+++ products/meetings/index.js
@@ -1,8 +1,19 @@
 import { HTML } from '@brtmvdl/frontend'
+import { HeaderComponent } from '../../assets/js/components/header.component.js'
+import { FooterComponent } from '../../assets/js/components/footer.component.js'
 
 export class Page extends HTML {
   onCreate() {
     super.onCreate()
-    this.setText('page')
+    this.append(new HeaderComponent())
+    this.append(this.getBody())
+    this.append(new FooterComponent())
   }
+
+  getBody() {
+    const html = new HTML()
+    html.setText('meetings')
+    return html
+  }
+
 }
