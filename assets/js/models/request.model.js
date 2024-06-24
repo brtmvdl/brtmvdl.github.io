70bb84f34dadd69c5a77071dc4678dbdf480b74a
Tarsis <br.tmvdl@gmail.com>
2024-06-23 21:15:24 -0300

    Updating app files

--- assets/js/models/request.model.js
+++ assets/js/models/request.model.js
@@ -4,16 +4,22 @@ export class RequestModel extends Model {
   name = ''
   method = ''
   url = ''
+  params = []
   query = []
   headers = []
 
-  constructor(name, method, url, query = [], headers = []) {
+  constructor(name, method, url, params = [], query = [], headers = []) {
     super()
     this.name = name
     this.method = method
     this.url = url
+    this.params = params
     this.query = query
     this.headers = headers
+  }
+
+  getUrl(params = {}) {
+    return Object.keys(params).reduce((url, param) => url.replace('{' + param + '}', params[param]), this.url)
   }
 
 }
