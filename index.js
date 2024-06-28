d8837cf9e06ed39c39c5d7789f5a9b95156979f4
Tarsis <br.tmvdl@gmail.com>
2024-06-28 11:43:48 -0300

    Updating app files

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { FooterComponent } from './assets/js/components/footer.component.js'
 import { LinkComponent } from './assets/js/components/link.component.js'
 import { TextComponent } from './assets/js/components/text.component.js'
 import products from './assets/js/lists/products.js'
-import projects from './assets/js/lists/projects.js'
+import experiences from './assets/js/lists/experiences.js'
 import { stepName } from './assets/js/utils/functions.js'
 
 export class Page extends HTML {
@@ -17,18 +17,18 @@ export class Page extends HTML {
 
   getBody() {
     const html = new HTML()
-    html.append(new TextComponent({ text: 'Donate' }))
+    html.append(new TextComponent({ text: 'donate' }))
     html.append(new LinkComponent({ text: 'donate', href: 'https://link.mercadopago.com.br/brtmvdl' }))
-    html.append(new TextComponent({ text: 'Social' }))
+    html.append(new TextComponent({ text: 'social' }))
     html.append(new LinkComponent({ text: 'email', href: 'mailto:br.tmvdl@gmail.com' }))
     html.append(new LinkComponent({ text: 'github', href: 'https://github.com/brtmvdl' }))
     html.append(new LinkComponent({ text: 'linkedin', href: 'https://www.linkedin.com/in/brtmvdl/' }))
     html.append(new LinkComponent({ text: 'twitter', href: 'https://twitter.com/brtmvdl' }))
     html.append(new LinkComponent({ text: 'discord', href: 'https://discord.gg/2zWpWBgmPj' }))
-    html.append(new TextComponent({ text: 'Products' }))
+    html.append(new TextComponent({ text: 'products' }))
     Array.from(products).map(({ id }) => html.append(new LinkComponent({ text: id, href: `/products/${id}/` })))
-    html.append(new TextComponent({ text: 'Projects' }))
-    Array.from(projects).map(({ id, step }) => html.append(new LinkComponent({ text: id + stepName(step, ' (', ') '), href: `/projects/${id}/` })))
+    html.append(new TextComponent({ text: 'experiences' }))
+    Array.from(experiences).map(({ id, step }) => html.append(new LinkComponent({ text: id + stepName(step, ' (', ') '), href: `/projects/${id}/` })))
     return html
   }
 }
