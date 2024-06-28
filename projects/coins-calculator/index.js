b9dfbe2705fca293bc4e9ba3f201336ef79b097a
Tarsis <br.tmvdl@gmail.com>
2024-06-27 23:51:14 -0300

    Updating app files

--- projects/coins-calculator/index.js
+++ projects/coins-calculator/index.js
@@ -1,54 +1,17 @@
-import { HTML, nH1, nLink, nButton, nFlex, nSpan, nInput } from '@brtmvdl/frontend'
-
-class SpanComponent extends nSpan {
-  text = ''
-
-  constructor(text = '') {
-    super()
-    this.text = text
-  }
-
-  onCreate() {
-    super.onCreate()
-    this.setText(this.text)
-  }
-}
-
-class nInputComponent extends nInput {
-  type = 'text'
-
-  constructor(type = 'text') {
-    super()
-    this.type = type
-  }
-
-  hasContainer() {
-    return false
-  }
-
-  onCreate() {
-    super.onCreate()
-    this.setStyles()
-    this.setAttr('type', this.type)
-  }
-
-  setStyles() {
-    this.setStyle('box-shadow', '0rem 0rem 0rem calc(1rem / 8) rgba(0, 0, 0, 0.5)')
-    // this.setStyle('margin', 'calc(-1rem / 4)')
-    this.setStyle('padding', 'calc(1rem / 4)')
-    this.setStyle('outline', 'none')
-    this.setStyle('border', 'none')
-  }
-}
+import { HTML, nFlex, nSpan } from '@brtmvdl/frontend'
+import { TextComponent } from '../../assets/js/components/text.component.js'
+import { InputComponent } from '../../assets/js/components/input.component.js'
+import { LinkComponent } from '../../assets/js/components/link.component.js'
+import { ButtonComponent } from '../../assets/js/components/button.component.js'
 
 export class Page extends HTML {
   children = {
-    symbol: new nInputComponent(),
-    buy_quantity_input: new nInputComponent('number'),
-    buy_price_input: new nInputComponent('number'),
-    sell_price_input: new nInputComponent('number'),
-    gain_price: new SpanComponent(),
-    gain_percent: new SpanComponent(),
+    symbol: new InputComponent({ placeholder: 'symbol' }),
+    buy_quantity_input: new InputComponent({ placeholder: 'quantity', type: 'number' }),
+    buy_price_input: new InputComponent({ placeholder: 'price', type: 'number' }),
+    sell_price_input: new InputComponent({ placeholder: 'price', type: 'number' }),
+    gain_price: new TextComponent(),
+    gain_percent: new TextComponent(),
     links: new HTML(),
   }
 
@@ -62,8 +25,8 @@ export class Page extends HTML {
     super.onCreate()
     this.setStyle('padding', '1rem')
     this.setStyle('margin', '0 auto')
-    this.append(this.getTitleH1())
-    this.appendFlex('When', this.getSymbolInput())
+    this.append(new TextComponent({ text: 'coins calculator' }))
+    this.appendFlex('when', this.getSymbolInput())
     this.appendFlex('is in', this.getBuyPriceInput())
     this.appendFlex('buy', this.getBuyQuantityInput())
     this.appendFlex('sell', this.getSellPriceInput())
@@ -74,40 +37,29 @@ export class Page extends HTML {
     this.append(this.children.links)
   }
 
-  getTitleH1() {
-    const h1 = new nH1()
-    h1.setText('Coins Calculator')
-    h1.setStyle('margin', '0rem')
-    return h1
-  }
-
   appendFlex(text = '', component = new HTML()) {
     const flex = new nFlex()
     flex.setStyle('margin', '1rem 0rem')
-    flex.append(new SpanComponent(text))
+    flex.append(new TextComponent({ text }))
     flex.append(component)
     this.append(flex)
   }
 
   getSymbolInput() {
-    this.children.symbol.setPlaceholder('symbol')
     return this.children.symbol
   }
 
   getBuyQuantityInput() {
-    this.children.buy_quantity_input.setPlaceholder('quantity')
     this.children.buy_quantity_input.on('keyup', () => this.calcGains())
     return this.children.buy_quantity_input
   }
 
   getBuyPriceInput() {
-    this.children.buy_price_input.setPlaceholder('price')
     this.children.buy_price_input.on('keyup', () => this.calcGains())
     return this.children.buy_price_input
   }
 
   getSellPriceInput() {
-    this.children.sell_price_input.setPlaceholder('price')
     this.children.sell_price_input.on('keyup', () => this.calcGains())
     return this.children.sell_price_input
   }
@@ -139,16 +91,7 @@ export class Page extends HTML {
   }
 
   getExportButton() {
-    const button = new nButton()
-    button.setText('export')
-    button.on('click', () => this.children.links.append(this.createDownloadLink(
-      this.children.symbol.getValue(),
-      this.children.sell_price_input.getValue(),
-      this.children.buy_quantity_input.getValue(),
-      this.children.buy_price_input.getValue(),
-      this.getPrice(),
-      this.getPercent(),
-    )))
+    const button = new ButtonComponent({ text: 'export', onclick: () => this.onExportButtonClick() })
     button.setContainerStyle('width', '100%')
     button.setStyle('box-sizing', 'border-box')
     button.setStyle('display', 'inline-block')
@@ -159,13 +102,23 @@ export class Page extends HTML {
     return button
   }
 
+  onExportButtonClick() {
+    this.children.links.append(this.createDownloadLink(
+      this.children.symbol.getValue(),
+      this.children.sell_price_input.getValue(),
+      this.children.buy_quantity_input.getValue(),
+      this.children.buy_price_input.getValue(),
+      this.getPrice(),
+      this.getPercent(),
+    ))
+  }
+
   createDownloadLink(symbol, sell_price, buy_quantity, buy_price, price, percent, filename = `${Date.now()}.json`) {
     const type = 'application/json'
     const lastModified = Date.now()
-    const link = new nLink()
+    const href = URL.createObjectURL(new File([new Blob([JSON.stringify({ symbol, sell_price, buy_quantity, buy_price, price, percent })], { type })], filename, { type, lastModified }))
+    const link = new LinkComponent({ text: filename, href })
     link.setAttr('download', filename)
-    link.href(URL.createObjectURL(new File([new Blob([JSON.stringify({ symbol, sell_price, buy_quantity, buy_price, price, percent })], { type })], filename, { type, lastModified })))
-    link.setText(filename)
     link.setStyle('box-sizing', 'border-box')
     link.setStyle('display', 'inline-block')
     link.setStyle('padding', '1rem 0rem')
