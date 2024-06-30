67a241ad48ccf338c9b5f1e3d4c3fcc266d29985
Tarsis <br.tmvdl@gmail.com>
2024-06-30 11:42:45 -0300

    Updating app files

--- projects/index.js
+++ projects/index.js
@@ -1,6 +1,10 @@
-import { HTML } from '@brtmvdl/frontend'
+import { HTML, nH1, nLink } from '@brtmvdl/frontend'
 import { HeaderComponent } from '../assets/js/components/header.component.js'
 import { FooterComponent } from '../assets/js/components/footer.component.js'
+import { LinkComponent } from '../assets/js/components/link.component.js'
+import { TextComponent } from '../assets/js/components/text.component.js'
+import experiences from '../assets/js/lists/experiences.js'
+import { stepName } from '../assets/js/utils/functions.js'
 
 export class Page extends HTML {
   onCreate() {
@@ -11,7 +15,9 @@ export class Page extends HTML {
   }
 
   getBody() {
-    return new HTML()
+    const html = new HTML()
+    html.append(new TextComponent({ text: 'experiences' }))
+    Array.from(experiences).map(({ id, step }) => html.append(new LinkComponent({ text: id + stepName(step, ' (', ') '), href: `/projects/${id}/` })))
+    return html
   }
-
 }
