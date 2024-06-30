d90a75171def12f3d0981fad0e56939ab4f638ed
Tarsis <br.tmvdl@gmail.com>
2024-06-30 13:58:13 -0300

    Updating app files

--- projects/coins-calculator/index.js
+++ projects/coins-calculator/index.js
@@ -1,10 +1,12 @@
 import { HTML, nFlex, nSpan } from '@brtmvdl/frontend'
-import { TextComponent } from '../../assets/js/components/text.component.js'
-import { InputComponent } from '../../assets/js/components/input.component.js'
-import { LinkComponent } from '../../assets/js/components/link.component.js'
+import { PaddingComponent } from '../../assets/js/components/padding.component.js'
 import { ButtonComponent } from '../../assets/js/components/button.component.js'
+// import { HeaderComponent } from '../../assets/js/components/header.component.js'
+import { InputComponent } from '../../assets/js/components/input.component.js'
+import { TextComponent } from '../../assets/js/components/text.component.js'
+import { LinkComponent } from '../../assets/js/components/link.component.js'
 
-export class Page extends HTML {
+export class Page extends PaddingComponent {
   children = {
     symbol: new InputComponent({ placeholder: 'symbol' }),
     buy_quantity_input: new InputComponent({ placeholder: 'quantity', type: 'number' }),
@@ -23,8 +25,6 @@ export class Page extends HTML {
 
   onCreate() {
     super.onCreate()
-    this.setStyle('padding', '1rem')
-    this.setStyle('margin', '0 auto')
     this.append(new TextComponent({ text: 'coins calculator' }))
     this.appendFlex('when', this.getSymbolInput())
     this.appendFlex('is in', this.getBuyPriceInput())
@@ -33,7 +33,7 @@ export class Page extends HTML {
     this.appendFlex('we get', new HTML())
     this.appendFlex('in price', this.getGainPriceHTML())
     this.appendFlex('in percent', this.getGainPercentHTML())
-    this.append(this.getExportButton())
+    this.append(new ButtonComponent({ text: 'export', onclick: () => this.onExportButtonClick() }))
     this.append(this.children.links)
   }
 
@@ -90,18 +90,6 @@ export class Page extends HTML {
     return (100 * price / buy_quantity).toFixed(4)
   }
 
-  getExportButton() {
-    const button = new ButtonComponent({ text: 'export', onclick: () => this.onExportButtonClick() })
-    button.setContainerStyle('width', '100%')
-    button.setStyle('box-sizing', 'border-box')
-    button.setStyle('display', 'inline-block')
-    button.setStyle('text-align', 'center')
-    button.setStyle('padding', '1rem')
-    button.setStyle('border', 'none')
-    button.setStyle('width', '100%')
-    return button
-  }
-
   onExportButtonClick() {
     this.children.links.append(this.createDownloadLink(
       this.children.symbol.getValue(),
@@ -115,14 +103,9 @@ export class Page extends HTML {
 
   createDownloadLink(symbol, sell_price, buy_quantity, buy_price, price, percent, filename = `${Date.now()}.json`) {
     const type = 'application/json'
-    const lastModified = Date.now()
-    const href = URL.createObjectURL(new File([new Blob([JSON.stringify({ symbol, sell_price, buy_quantity, buy_price, price, percent })], { type })], filename, { type, lastModified }))
+    const href = URL.createObjectURL(new File([new Blob([JSON.stringify({ symbol, sell_price, buy_quantity, buy_price, price, percent })], { type })], filename, { type }))
     const link = new LinkComponent({ text: filename, href })
     link.setAttr('download', filename)
-    link.setStyle('box-sizing', 'border-box')
-    link.setStyle('display', 'inline-block')
-    link.setStyle('padding', '1rem 0rem')
-    link.setStyle('width', '100%')
     return link
   }
 }
