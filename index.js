8e7b941c1eb7f6a48801001bbcc9c5797993216e
Tarsis <br.tmvdl@gmail.com>
2024-06-30 13:56:59 -0300

    Updating app files

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-import { HTML, nH1, nLink } from '@brtmvdl/frontend'
-import { HeaderComponent } from './assets/js/components/header.component.js'
-import { FooterComponent } from './assets/js/components/footer.component.js'
+import { HTML } from '@brtmvdl/frontend'
 import { LinkComponent } from './assets/js/components/link.component.js'
 import { TextComponent } from './assets/js/components/text.component.js'
+import { PaddingComponent } from './assets/js/components/padding.component.js'
 import experiences from './assets/js/lists/experiences.js'
-import { stepName } from './assets/js/utils/functions.js'
 
-export class Page extends HTML {
+export class Page extends PaddingComponent {
   onCreate() {
     super.onCreate()
-    this.append(new HeaderComponent())
     this.append(this.getBody())
-    this.append(new FooterComponent())
   }
 
   getBody() {
     const html = new HTML()
-    html.append(new TextComponent({ text: 'donate' }))
-    html.append(new LinkComponent({ text: 'donate', href: 'https://link.mercadopago.com.br/brtmvdl' }))
     html.append(new TextComponent({ text: 'social' }))
     html.append(new LinkComponent({ text: 'email', href: 'mailto:br.tmvdl@gmail.com' }))
     html.append(new LinkComponent({ text: 'github', href: 'https://github.com/brtmvdl' }))
@@ -25,7 +19,7 @@ export class Page extends HTML {
     html.append(new LinkComponent({ text: 'twitter', href: 'https://twitter.com/brtmvdl' }))
     html.append(new LinkComponent({ text: 'discord', href: 'https://discord.gg/2zWpWBgmPj' }))
     html.append(new TextComponent({ text: 'experiences' }))
-    Array.from(experiences).map(({ id, step }) => html.append(new LinkComponent({ text: id + stepName(step, ' (', ') '), href: `/projects/${id}/` })))
+    Array.from(experiences).filter(({ step }) => step >= 3).map(({ id }) => html.append(new LinkComponent({ text: id, href: `/projects/${id}/` })))
     return html
   }
 }
