b808318d4c774a2ab94510e2f09fbaf5bd68373b
Tarsis <br.tmvdl@gmail.com>
2024-06-30 11:45:39 -0300

    Updating app files

--- projects/facebook-rest-api/components/messages.component.js
+++
@@ -1,9 +0,0 @@
-import * as Components from '../../../assets/js/components/messages.component.js'
-import { MessageCardComponent } from '../../../assets/js/components/message.card.component.js'
-import { MessageModel } from '../../../assets/js/models/message.model.js'
-
-export class MessagesComponent extends Components.MessagesComponent {
-  getMessageCardComponent(message = new MessageModel()) {
-    return new MessageCardComponent(message)
-  }
-}
