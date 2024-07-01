7e3f3589dcd7eb050023a103e9578498aaff09a7
Tarsis <br.tmvdl@gmail.com>
2024-07-01 18:59:31 -0300

    Updating app files

---
+++ assets/js/utils/api.js
@@ -0,0 +1,15 @@
+// 
+
+const url = (url, query = {}) => {
+  const nUrl = new URL(url)
+  Object.keys(query).map((q) => nUrl.searchParams.set(q, query[q]))
+  return nUrl.toString()
+}
+
+export const rest = {
+  musixmatch: {
+    v1: {
+      call: (method, path, params = {}) => fetch(url('http://api.musixmatch.com/ws/1.1/' + path, params), { method, mode: 'no-cors' }).then((res) => res.json())
+    }
+  }
+}
