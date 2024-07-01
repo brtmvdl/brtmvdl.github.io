5f6365f5aa3fa91dce055864a8bd1d4bb03505b6
Tarsis <br.tmvdl@gmail.com>
2024-07-01 11:03:52 -0300

    Updating app files

--- projects/voicerss-rest-api/components/audio.message.component.js
+++ projects/voicerss-rest-api/components/audio.message.component.js
@@ -1,15 +1,19 @@
 import { MessageComponent } from './message.component.js'
+import { TextComponent } from '../../../assets/js/components/text.component.js'
 import { nAudio } from './audio.js'
 
 export class AudioMessageComponent extends MessageComponent {
   onCreate() {
     super.onCreate()
-    this.append(this.getAudio())
+    this.append(this.getTextComponent())
+    this.append(this.getAudioComponent())
   }
 
-  getAudio() {
-    console.log('url', this.message.url)
+  getTextComponent() {
+    return new TextComponent({ text: this.message.text })
+  }
+
+  getAudioComponent() {
     return new nAudio(this.message.url)
   }
-
 }
