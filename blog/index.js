4a809b0d42d70011aa3b29a8d85f3e3a4c082f2a
Tarsis <br.tmvdl@gmail.com>
2024-07-03 19:37:29 -0300

    Updating app files

--- blog/index.js
+++ blog/index.js
@@ -1,17 +1,4 @@
 import { HTML } from '@brtmvdl/frontend'
-import { HeaderComponent } from '../assets/js/components/header.component.js'
-import { FooterComponent } from '../assets/js/components/footer.component.js'
+import { PaddingComponent } from '../assets/js/components/padding.component.js'
 
-export class Page extends HTML {
-  onCreate() {
-    super.onCreate()
-    this.append(new HeaderComponent())
-    this.append(this.getBody())
-    this.append(new FooterComponent())
-  }
-
-  getBody() {
-    return new HTML()
-  }
-
-}
+export class Page extends PaddingComponent { }
