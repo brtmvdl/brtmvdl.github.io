fe31d209813dda571669a15fc286e44aa293d80b
Tarsis <br.tmvdl@gmail.com>
2024-07-04 10:34:58 -0300

    Updating app files

---
+++ assets/js/utils/peer.js
@@ -0,0 +1,59 @@
+import { Peer } from 'https://esm.sh/peerjs@1.5.4?bundle-deps'
+import { qrcode } from './functions.js'
+
+export const getPeerConnection = () => { }
+
+export const createControlsUrl = (project, id) => {
+  const url = new URL(window.location)
+  url.pathname = `/projects/${project}/controls.html?id=${id}`
+  return url.toString()
+}
+
+export const createQrcodeImage = (url) => {
+  console.log('url', (url).replace('%3F', '?'))
+  const image = document.createElement('img')
+  image.src = qrcode(url)
+  image.style.position = 'fixed'
+  image.style.left = '1rem'
+  image.style.bottom = '1rem'
+  document.body.append(image)
+}
+
+export const createNewPeer = (project) => {
+  const peer = new Peer()
+
+  peer.on('connection', (conn) => {
+    console.log('peer connection', { peer, conn })
+
+    conn.on('open', (open) => {
+      console.log('conn open', { peer, conn, open })
+    })
+
+    conn.on('close', (close) => {
+      console.log('conn close', { peer, conn, close })
+    })
+
+    conn.on('error', (error) => {
+      console.log('conn error', { peer, conn, error })
+    })
+
+    conn.on('data', (data) => {
+      console.log('conn data', { peer, conn, data })
+    })
+  })
+
+  peer.on('open', (open) => {
+    console.log('peer open', { peer, open })
+    createQrcodeImage(createControlsUrl(project, peer.id))
+  })
+
+  peer.on('error', (error) => {
+    console.log('peer error', { peer, error })
+  })
+
+  peer.on('close', (close) => {
+    console.log('peer close', { peer, close })
+  })
+
+  return peer
+}
--- projects/airplane/index.js
+++ projects/airplane/index.js
@@ -1,28 +1,12 @@
 import * as THREE from 'three'
-import { qrcode } from '../../assets/js/utils/functions.js'
-import { Peer } from 'https://esm.sh/peerjs@1.5.4?bundle-deps'
+import { createNewPeer } from '../../assets/js/utils/peer.js'
+import * as COLORS from '../../assets/js/utils/colors.js'
 
 const __ = {
   getWidth: () => window.innerWidth,
   getHeight: () => window.innerHeight,
   getAspect: () => __.getWidth() / __.getHeight(),
-  createControlsUrl: (id) => {
-    const url = new URL(window.location)
-    url.pathname = `/projects/airplane/controls.html?id=${id}`
-    return url.toString()
-  },
-  createQrcodeImage: (url) => {
-    console.log('url', (url).replace('%3F', '?'))
-    const image = document.createElement('img')
-    image.src = qrcode(url)
-    image.style.position = 'fixed'
-    image.style.left = '1rem'
-    image.style.bottom = '1rem'
-    document.body.append(image)
-  }
 }
-
-const COLORS = { YELLOW: 0xFFFF00, WHITE: 0xFFFFFF, }
 
 const scene = new THREE.Scene()
 
@@ -41,43 +25,19 @@ renderer.setAnimationLoop(() => {
 })
 
 const params = {
-  up: () => camera.position.y += 0.1,
-  down: () => camera.position.y -= 0.1,
-  left: () => camera.position.x += 0.1,
-  right: () => camera.position.x -= 0.1,
-  front: () => camera.position.z += 0.1,
-  down: () => camera.position.z -= 0.1,
+  up: () => camera.position.y += +0.1,
+  down: () => camera.position.y += -0.1,
+  left: () => camera.position.x += -0.1,
+  right: () => camera.position.x += +0.1,
+  front: () => camera.position.z += -0.1,
+  back: () => camera.position.z += +0.1,
 }
 
-const peer = new Peer()
+const peer = createNewPeer('airplane')
 
 peer.on('connection', function (conn) {
-  console.log('peer connection', { peer, conn })
-
-  conn.on('open', function (open) {
-    console.log('conn open', { peer, open })
-  })
-
-  conn.on('close', function (close) {
-    console.log('conn close', { peer, close })
-  })
-
-  conn.on('error', function (error) {
-    console.log('conn error', { peer, error })
-  })
-
   conn.on('data', function ({ text, fn } = {}) {
     console.log('conn data', { peer, text, fn })
-
     params[fn]?.()
   })
 })
-
-peer.on('open', () => {
-  console.log('peer open')
-  __.createQrcodeImage(__.createControlsUrl(peer.id))
-})
-
-peer.on('error', () => console.log('peer error'))
-
-peer.on('close', () => console.log('peer close'))
