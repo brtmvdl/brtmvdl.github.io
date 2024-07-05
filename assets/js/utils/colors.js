1d5f3938ed7a8230caf872bd59f565fd48f4505f
Tarsis <br.tmvdl@gmail.com>
2024-07-05 10:51:16 -0300

    Updating app files

--- assets/js/utils/colors.js
+++ assets/js/utils/colors.js
@@ -1,5 +1,11 @@
 // 
 
-export const YELLOW = 0xFFFF00
+export const RED_1 = 0xFF0000
 
-export const WHITE = 0xFFFFFF
+export const YELLOW_1 = 0xFFFF00
+
+export const GREEN_1 = 0x00FF00
+
+export const WHITE_1 = 0xFFFFFF
+
+export const BLACK_1 = 0x000000
