bb633899671272fa498c465d47aafdede99c943e
Tarsis <br.tmvdl@gmail.com>
2024-07-09 14:34:02 -0300

    Updating app files

--- projects/slots/index.js
+++ projects/slots/index.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import * as COLORS from '../../assets/js/utils/colors.js'
+import { createCylinderGeometry } from './geometries/cylinder.geometry.js'
 import { radian } from '../../assets/js/utils/3d.js'
 
 const scene = new THREE.Scene()
@@ -15,44 +15,12 @@ const state = { running: false, }
 const randomNum = (num = 100) => Math.floor(Math.random() * num) + 1
 
 const cylinders = Array.from(Array(+3.0)).map((_, ix) => {
-  const c = new THREE.Mesh(
-    new THREE.CylinderGeometry(+1.0, +1.0, +1.0, +8.0, +1.0, true),
-    new THREE.MeshBasicMaterial({ color: COLORS.WHITE_1 }),
-  )
-
-  c.position.set((ix * +1.5) - +1.5, +1.0, +1.0)
+  const c = createCylinderGeometry({ planeSize: +10.0 })
+  c.position.set((ix * +10) - +10, +0.0, +1.0)
   c.rotation.set(+0.0, +0.0, radian(+90.0))
   return c
 })
 Array.from(cylinders).map((c) => scene.add(c))
-
-//
-
-const rotateCylinders = () => {
-  if (!state.running) {
-    state.running = true
-    Array.from(cylinders).map((c, ix) => {
-      const r = +50.0 + randomNum(+100.0)
-      let i = 0
-      const id = setInterval(() => {
-        c.rotation.x += (radian((+90.0 * i++) / (params.segments / 4))) % radian(+360.0)
-        const { x, y, z } = c.rotation
-        console.log([ix, x, y, z])
-        if (i > r) {
-          clearInterval(id)
-          state.running = false
-        }
-      }, 50)
-    })
-  }
-}
-
-window.addEventListener('keypress', ({ key }) => {
-  switch (key) {
-    case 'Enter': return rotateCylinders()
-  }
-  console.log('keypress', ({ key }))
-})
 
 //
 
