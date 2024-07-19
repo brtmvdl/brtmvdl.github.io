a368a33151401df872c64816f8ee3dfc965121c2
Tarsis <br.tmvdl@gmail.com>
2024-07-19 11:04:02 -0300

    Updating app files

--- blog/index.js
+++ blog/index.js
@@ -1,4 +1,9 @@
-import { HTML } from '../assets/js/libs/frontend/index.js'
 import { PaddingComponent } from '../assets/js/components/padding.component.js'
+import { TextComponent } from '../assets/js/components/text.component.js'
 
-export class Page extends PaddingComponent { }
+export class Page extends PaddingComponent {
+  onCreate() {
+    super.onCreate()
+    this.append(new TextComponent({ text: 'blog' }))
+  }
+}
