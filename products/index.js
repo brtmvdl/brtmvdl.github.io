8f485bc626cade1866753307f17d3245d368a49a
Tarsis <br.tmvdl@gmail.com>
2024-07-19 11:04:09 -0300

    Updating app files

---
+++ products/index.js
@@ -0,0 +1,9 @@
+import { PaddingComponent } from '../assets/js/components/padding.component.js'
+import { TextComponent } from '../assets/js/components/text.component.js'
+
+export class Page extends PaddingComponent {
+  onCreate() {
+    super.onCreate()
+    this.append(new TextComponent({ text: 'products' }))
+  }
+}
