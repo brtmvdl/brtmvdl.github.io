0ab696be2972efe11aa0509923c85a114fa416e3
Tarsis <br.tmvdl@gmail.com>
2024-07-23 11:18:17 -0300

    Updating app files

---
+++ config.js
@@ -0,0 +1,5 @@
+
+export const google = {
+  client_id: '387615697147',
+  api_key: 'AIzaSyBNuupykoCanqmDHtksxEWvDn7wJo4JmgA'
+}
--- projects/youtube-live-streaming-api/index.js
+++ projects/youtube-live-streaming-api/index.js
@@ -2,6 +2,7 @@ import { PaddingComponent } from '../../assets/js/components/padding.component.j
 import { ButtonComponent } from '../../assets/js/components/button.component.js'
 import { TextComponent } from '../../assets/js/components/text.component.js'
 import { InputComponent } from '../../assets/js/components/input.component.js'
+import { google } from '../../config.js'
 
 export class Page extends PaddingComponent {
   children = {
@@ -11,19 +12,18 @@ export class Page extends PaddingComponent {
   onCreate() {
     super.onCreate()
     this.append(new TextComponent({ text: 'youtube live streaming api' }))
-    this.append(this.getClientIdInputComponent())
+    this.append(new ButtonComponent({ text: 'authorize', onclick: () => this.onAuthorizeButtonClick() }))
     this.append(new ButtonComponent({ text: 'authenticate', onclick: () => this.onAuthenticateButtonClick() }))
   }
 
-  getClientIdInputComponent() {
-    return this.children.client_id
-  }
-
-  getClientId() {
-    return this.children.client_id.getValue()
+  onAuthorizeButtonClick() {
+    gapi.load('client:auth2', () => { gapi.auth2.init({ client_id: google.client_id }) })
   }
 
   onAuthenticateButtonClick() {
-    gapi.load('client:auth2', () => { gapi.auth2.init({ client_id: this.getClientId() }) })
+    gapi.client.setApiKey(google.api_key)
+    gapi.client.load('https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest')
+    gapi.auth2.getAuthInstance().signIn({ scope: 'https://www.googleapis.com/auth/youtube.force-ssl' })
   }
+
 }
