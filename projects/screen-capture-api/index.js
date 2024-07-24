657897540019c03c1ec8fe272cc2855c4181f297
Tarsis <br.tmvdl@gmail.com>
2024-07-24 16:28:23 -0300

    Updating app files

--- projects/screen-capture-api/index.js
+++ projects/screen-capture-api/index.js
@@ -1,33 +1,23 @@
-import { HTML, nButton, nVideo } from '../../assets/js/libs/frontend/index.js'
-
+import { HTML, nVideo } from '../../assets/js/libs/frontend/index.js'
+import { PaddingComponent } from '../../assets/js/components/padding.component.js'
+import { ButtonComponent } from '../../assets/js/components/button.component.js'
+import { GOOGLE } from './../../assets/js/utils/googleusercontent.js'
 import { API_KEY } from './config.js'
 
-import { GOOGLE } from './../../assets/js/utils/googleusercontent.js'
-
-class nScreenButton extends nButton {
-  onCreate() {
-    this.setStyle('background-color', '#000000')
-    // this.setStyle('color', '#ffffff')
-    this.setStyle('padding', '1rem')
-    this.setStyle('margin', '1rem')
-    this.setStyle('border', 'none')
-  }
-}
-
-export class Page extends HTML {
+export class Page extends PaddingComponent {
 
   children = {
     videoElem: new nVideo(),
     log: new HTML(),
-    startCaptureButton: new nScreenButton(),
-    stopCaptureButton: new nScreenButton(),
-    startRecordButton: new nScreenButton(),
-    stopRecordButton: new nScreenButton(),
-    authenticateButton: new nScreenButton(),
-    loadClientButton: new nScreenButton(),
-    executeButton: new nScreenButton(),
-    insertButton: new nScreenButton(),
-    loadButton: new nScreenButton(),
+    startCaptureButton: new ButtonComponent({ text: 'start capture' }),
+    stopCaptureButton: new ButtonComponent({ text: 'stop capture' }),
+    startRecordButton: new ButtonComponent({ text: 'start Record' }),
+    stopRecordButton: new ButtonComponent({ text: 'stop record' }),
+    authenticateButton: new ButtonComponent({ text: 'authenticate' }),
+    loadClientButton: new ButtonComponent({ text: 'load Client' }),
+    executeButton: new ButtonComponent({ text: 'execute' }),
+    insertButton: new ButtonComponent({ text: 'insert' }),
+    loadButton: new ButtonComponent({ text: 'load' }),
   }
 
   state = {
@@ -42,6 +32,7 @@ export class Page extends HTML {
   }
 
   onCreate() {
+    super.onCreate()
     this.setText('page')
     this.append(this.getVideoElem())
     this.append(this.getStartCaptureButton())
@@ -57,38 +48,26 @@ export class Page extends HTML {
   }
 
   getVideoElem() {
-    console.log('getVideoElem')
-
     return this.children.videoElem
   }
 
   getLog() {
-    console.log('getLog')
-
     return this.children.log
   }
 
   getStartCaptureButton() {
-    console.log('getStartCaptureButton')
-
     this.children.startCaptureButton.setText('start capture')
     this.children.startCaptureButton.addEventListener('click', () => this.startCapture())
-
     return this.children.startCaptureButton
   }
 
   getStopCaptureButton() {
-    console.log('getStopCaptureButton')
-
     this.children.stopCaptureButton.setText('stop capture')
     this.children.stopCaptureButton.addEventListener('click', () => this.stopCapture())
-
     return this.children.stopCaptureButton
   }
 
   startCapture() {
-    console.log('startCapture')
-
     const self = this
     self.children.log.setText('')
 
@@ -102,51 +81,34 @@ export class Page extends HTML {
   }
 
   stopCapture() {
-    console.log('stopCapture')
-
     this.children.videoElem.getSrcObject().getTracks().forEach((t) => t.stop())
     this.children.videoElem.setSrcObject(null)
   }
 
   getStartRecordButton() {
-    console.log('getStartRecordButton')
-
     this.children.startRecordButton.setText('start Record')
     this.children.startRecordButton.addEventListener('click', () => this.startRecord())
-
     return this.children.startRecordButton
   }
 
   getStopRecordButton() {
-    console.log('getStopRecordButton')
-
     this.children.stopRecordButton.setText('stop Record')
     this.children.stopRecordButton.addEventListener('click', () => this.stopRecord())
-
     return this.children.stopRecordButton
   }
 
   setRecorder() {
-    console.log('setRecorder')
-
     this.state.recorder = new MediaRecorder(this.state.src, { mimeType: 'video/webm' })
-
     this.state.recorder.addEventListener('dataavailable', (e) => this.onRecorderDataAvailable(e))
     this.state.recorder.addEventListener('stop', (e) => this.onRecorderStop(e))
   }
 
   onRecorderDataAvailable(e) {
-    console.log('onRecorderDataAvailable', e)
-
     this.state.chunks.push(e.data)
   }
 
   onRecorderStop(e) {
-    console.log('recorder stop', e)
-
     const blob = new Blob(this.state.chunks, { type: 'video/webm' })
-    console.log('recorder blob', blob)
-
     this.saveBlob(blob)
   }
 
@@ -155,61 +117,43 @@ export class Page extends HTML {
   }
 
   startRecord() {
-    console.log('startRecord')
-
     this.state.recorder?.start()
   }
 
   stopRecord() {
-    console.log('stopRecord')
-
     this.state.recorder?.stop()
   }
 
 
   authenticate() {
-    console.log('authenticate')
-
     return new Promise((s, f) => gapi.auth2.getAuthInstance().signIn({
       scope: this.state.scope
     }).then(s).catch(f))
   }
 
   getAuthenticateButton() {
-    console.log('getAuthenticateButton')
-
     this.children.authenticateButton.setText('authenticate')
     this.children.authenticateButton.addEventListener('click', () => this.authenticate().then(console.log).catch(console.error))
-
     return this.children.authenticateButton
   }
 
   loadClient() {
-    console.log('loadClient')
-
     const url = 'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'
     return new Promise((s, f) => gapi.client.load(url).then(s).catch(f))
   }
 
   getLoadClientButton() {
-    console.log('getLoadClientButton')
-
     this.children.loadClientButton.setText('load client')
     this.children.loadClientButton.addEventListener('click', () => this.loadClient().then(console.log).catch(console.error))
-
     return this.children.loadClientButton
   }
 
   execute() {
-    console.log('execute')
-
     const b = { id: this.state.broadcastId, part: ['snippet'], streamId: this.state.streamId }
     return new Promise((s, f) => gapi.client.youtube.liveBroadcasts.bind(b).then(s).catch(f))
   }
 
   getExecuteButton() {
-    console.log('getExecuteButton')
-
     this.children.executeButton.setText('execute')
     this.children.executeButton.addEventListener('click', () => this.execute().then(console.log).catch(console.error))
 
@@ -217,8 +161,6 @@ export class Page extends HTML {
   }
 
   insert() {
-    console.log('insert')
-
     const scheduledStartTime = (new Date()).toISOString()
     const title = `youtube.liveBroadcasts-${scheduledStartTime}`
 
@@ -228,35 +170,24 @@ export class Page extends HTML {
   }
 
   getInsertButton() {
-    console.log('getInsertButton')
-
     this.children.insertButton.setText('insert')
     this.children.insertButton.addEventListener('click', () => this.insert().then(console.log).catch(console.error))
-
     return this.children.insertButton
   }
 
   auth2init() {
-    console.log('auth2init')
-
     const client_id = GOOGLE.client_id
     const redirect_uri = (window.location).toString()
-    console.log('auth2init', { client_id, redirect_uri })
     gapi.auth2.init({ client_id, redirect_uri }, console.log)
   }
 
   loadGAPI() {
-    console.log('loadGAPI')
-
     gapi.load('client:auth2', () => this.auth2init())
   }
 
   getLoadButton() {
-    console.log('getLoadButton')
-
     this.children.loadButton.setText('load GAPI')
     this.children.loadButton.addEventListener('click', () => this.loadGAPI())
-
     return this.children.loadButton
   }
 }
