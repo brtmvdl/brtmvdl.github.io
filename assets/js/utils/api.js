79cbf1ada4af534816f770395cae872fccdb049e
Tarsis <br.tmvdl@gmail.com>
2024-07-27 18:20:17 -0300

    Updating app files

--- assets/js/utils/api.js
+++ assets/js/utils/api.js
@@ -1,9 +1,15 @@
+import * as GOOGLE from './googleusercontent.js'
+
 import { request } from './ajax.js'
 
 const url = (url, query = {}) => {
   const nUrl = new URL(url)
   Object.keys(query).map((q) => nUrl.searchParams.set(q, query[q]))
   return nUrl.toString()
+}
+
+export const urls = {
+  youtube: (pathname, params = {}) => url('https://www.googleapis.com/youtube/v3/' + pathname, params)
 }
 
 export const rest = {
@@ -15,6 +21,13 @@ export const rest = {
   newsapi: {
     v2: {
       call: (path, params = {}) => request('GET', url('https://newsapi.org/v2/' + path, params)),
+    }
+  },
+  youtube: {
+    v3: {
+      liveBroadcasts: {
+        insert: (params = {}, body = {}) => request('POST', urls.youtube('liveBroadcasts', params), body, { Authorization: `Bearer ${GOOGLE.api_key}` }),
+      }
     }
   }
 }
