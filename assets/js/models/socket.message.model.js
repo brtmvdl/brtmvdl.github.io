96c599fe0ab229284b7db0a491d6c9f83c1ac388
Tarsis <br.tmvdl@gmail.com>
2024-08-01 18:37:03 -0300

    Updating app files

--- assets/js/models/socket.message.model.js
+++ assets/js/models/socket.message.model.js
@@ -1,3 +1,5 @@
 import { MessageModel } from './message.model.js'
 
-export class SocketMessageModel extends MessageModel { }
+export class SocketMessageModel extends MessageModel {
+  Side = 'socket'
+}
