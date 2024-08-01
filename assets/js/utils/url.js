7eed1b8018e68529bed63f11f2f2a2dd133b2622
Tarsis <br.tmvdl@gmail.com>
2024-08-01 19:22:46 -0300

    Updating app files

--- assets/js/utils/url.js
+++ assets/js/utils/url.js
@@ -1,6 +1,6 @@
 export const u = new URL(window.location)
 
-export const usp = new URLSearchParams(u.search)
+export const usp = new URLSearchParams(u.searchParams)
 
 export const getURLSearchParam = (name, def = '') => usp.get(name) || def
 
@@ -11,3 +11,9 @@ export const getParams = ({ color = '000000', bgcolor = 'ffffff', margin = '0rem
   title: getURLSearchParam('title', title),
   id: getURLSearchParam('id', id)
 })
+
+export const createURL = ({ search = {} } = {}) => {
+  const url = new URL(window.location)
+  Object.keys(search).map((key) => url.searchParams.set(key, search[key]))
+  return url.toString()
+}
