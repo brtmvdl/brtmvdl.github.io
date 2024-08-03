63a63096905ac3169a40c5104fd4252c23512b40
Tarsis <br.tmvdl@gmail.com>
2024-08-03 11:15:09 -0300

    Updating app files

--- assets/js/utils/functions.js
+++ assets/js/utils/functions.js
@@ -4,18 +4,6 @@ export const uuid = () => Date.now().toString() + (Math.floor((10 ** 20 * Math.r
 
 export const qrcode = (data = window.location.toString(), size = '150x150') => `https://api.qrserver.com/v1/create-qr-code/?size=${size}&data=${data}`
 
-export const months = {
-  'enus': ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-}
-
-export const monthName = (month, language = 'enus') => months[language]?.[month - 1]
-
 export const steps = { '-1': 'comming soon', '0': 'alpha', '1': 'beta', '2': 'staging' }
 
 export const stepName = (step, prefix = '', suffix = '') => (step in steps) ? (prefix + steps[step] + suffix) : ''
-
-export const innerWidth = () => window.innerWidth
-
-export const innerHeight = () => window.innerHeight
-
-export const innerAspect = () => innerWidth() / innerHeight()
